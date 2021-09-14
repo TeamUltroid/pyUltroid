327c24bd6957cec51fb67e3f19bdff4b7e3b125b
TeamUltroid <teamultroid@ultroid.tech>
2021-09-14 18:02:26 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/__init__.py
+++ pyUltroid/functions/__init__.py
@@ -4,5 +4,3 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
-
-from .. import *
