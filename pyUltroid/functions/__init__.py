375a22ae312c53ec8bc1dd27211e9f521a72b3c0
Devesh Pal <newdev0@outlook.com>
2021-09-15 11:00:23 +0530

    Update __init__.py

--- pyUltroid/functions/__init__.py
+++ pyUltroid/functions/__init__.py
@@ -4,3 +4,5 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+from .. import LOGS, ultroid_bot  #ignore: pylint
