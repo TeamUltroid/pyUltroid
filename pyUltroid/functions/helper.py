a4b361ef7fd74d602e27d0b22636ae2d2a246e59
Devesh Pal <newdev0@outlook.com>
2021-09-15 10:08:52 +0530

    Create helper.py

---
+++ pyUltroid/functions/helper.py
@@ -0,0 +1,287 @@
+import asyncio
+from git import Repo
+from . import LOGS
+from ..dB._database import Var
+from .FastTelethon import download_file as downloadable
+from .FastTelethon import upload_file as uploadable
+
+from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
+
+
+UPSTREAM_REPO_URL = Repo().remotes[0].config_reader.get("url").replace(".git", "")
+
+
+async def bash(cmd):
+    process = await asyncio.create_subprocess_shell(
+        cmd,
+        stdout=asyncio.subprocess.PIPE,
+        stderr=asyncio.subprocess.PIPE,
+    )
+    stdout, stderr = await process.communicate()
+    err = stderr.decode().strip()
+    out = stdout.decode().strip()
+    return out, err
+
+# ---------------Updater---------------
+# Will add in class
+
+
+async def updateme_requirements():
+    """To Update requirements"""
+    try:
+        _, __ = await bash(f"{sys.executable} -m pip install --no-cache-dir -r {reqs}")
+    except Exception:
+        return format_exc()
+
+
+def gen_chlog(repo, diff):
+    ac_br = repo.active_branch.name
+    ch_log = tldr_log = ""
+    ch = f"<b>Ultroid {ultroid_version} updates for <a href={UPSTREAM_REPO_URL}/tree/{ac_br}>[{ac_br}]</a>:</b>"
+    ch_tl = f"Ultroid {ultroid_version} updates for {ac_br}:"
+    d_form = "%d/%m/%y || %H:%M"
+    for c in repo.iter_commits(diff):
+        ch_log += f"\n\n💬 <b>{c.count()}</b> 🗓 <b>[{c.committed_datetime.strftime(d_form)}]</b>\n<b><a href={UPSTREAM_REPO_URL.rstrip('/')}/commit/{c}>[{c.summary}]</a></b> 👨‍💻 <code>{c.author}</code>"
+        tldr_log += f"\n\n💬 {c.count()} 🗓 [{c.committed_datetime.strftime(d_form)}]\n[{c.summary}] 👨‍💻 {c.author}"
+    if ch_log:
+        return str(ch + ch_log), str(ch_tl + tldr_log)
+    return ch_log, tldr_log
+
+
+def updater(check_updates=False):
+    off_repo = UPSTREAM_REPO_URL
+    try:
+        repo = Repo()
+    except NoSuchPathError as error:
+        LOGS.info(f"`directory {error} is not found`")
+        repo.__del__()
+        return
+    except GitCommandError as error:
+        LOGS.info(f"`Early failure! {error}`")
+        repo.__del__()
+        return
+    except InvalidGitRepositoryError:
+        repo = Repo.init()
+        origin = repo.create_remote("upstream", off_repo)
+        origin.fetch()
+        repo.create_head("main", origin.refs.main)
+        repo.heads.main.set_tracking_branch(origin.refs.main)
+        repo.heads.main.checkout(True)
+    ac_br = repo.active_branch.name
+    try:
+        repo.create_remote("upstream", off_repo)
+    except Exception as er:
+        LOGS.info(er)
+    ups_rem = repo.remote("upstream")
+    ups_rem.fetch(ac_br)
+    changelog, tl_chnglog = gen_chlog(repo, f"HEAD..upstream/{ac_br}")
+    return bool(changelog)
+
+
+# ----------------Fast Upload/Download----------------
+# @1danish_00 @new-dev0 @buddhhu
+
+
+async def uploader(file, name, taime, event, msg):
+    with open(file, "rb") as f:
+        result = await uploadable(
+            client=event.client,
+            file=f,
+            filename=name,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d,
+                    t,
+                    event,
+                    taime,
+                    msg,
+                ),
+            ),
+        )
+    return result
+
+
+async def downloader(filename, file, event, taime, msg):
+    with open(filename, "wb") as fk:
+        result = await downloadable(
+            client=event.client,
+            location=file,
+            out=fk,
+            progress_callback=lambda d, t: asyncio.get_event_loop().create_task(
+                progress(
+                    d,
+                    t,
+                    event,
+                    taime,
+                    msg,
+                ),
+            ),
+        )
+    return result
+
+
+# ~~~~~~~~~~~~~~~~~~~~DDL Downloader~~~~~~~~~~~~~~~~~~~~
+# @buddhhu @new-dev0
+
+
+async def download_file(link, name):
+    """for files, without progress callback with aiohttp"""
+    async with aiohttp.ClientSession() as ses:
+        async with ses.get(link) as re_ses:
+            file = await aiofiles.open(name, "wb")
+            await file.write(await re_ses.read())
+            await file.close()
+    return name
+
+
+async def fast_download(download_url, filename=None, progress_callback=None):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(download_url, timeout=None) as response:
+            if not filename:
+                filename = download_url.rpartition("/")[-1]
+            total_size = int(response.headers.get("content-length", 0)) or None
+            downloaded_size = 0
+            with open(filename, "wb") as f:
+                async for chunk in response.content.iter_chunked(1024):
+                    if chunk:
+                        f.write(chunk)
+                        downloaded_size += len(chunk)
+                    if progress_callback:
+                        await _maybe_await(
+                            progress_callback(downloaded_size, total_size)
+                        )
+            return filename
+
+# ------------------Some Small Funcs----------------
+
+
+def time_formatter(milliseconds):
+    to_return = ""
+    minutes, seconds = divmod(int(milliseconds / 1000), 60)
+    hours, minutes = divmod(minutes, 60)
+    days, hours = divmod(hours, 24)
+    weeks, days = divmod(days, 7)
+    to_return = (
+        ((str(weeks) + "w:") if weeks else "")
+        + ((str(days) + "d:") if days else "")
+        + ((str(hours) + "h:") if hours else "")
+        + ((str(minutes) + "m:") if minutes else "")
+        + ((str(seconds) + "s") if seconds else "")
+    )
+    if to_return == "":
+        "0 s"
+    elif tmp.endswith(":"):
+        to_return[:-1]
+    else:
+        to_return
+
+
+def humanbytes(size):
+    if size in [None, ""]:
+        return "0 B"
+    for unit in ["B", "KB", "MB", "GB"]:
+        if size < 1024:
+            break
+        size /= 1024
+    return f"{size:.2f} {unit}"
+
+async def progress(current, total, event, start, type_of_ps, file_name=None):
+    diff = time.time() - start
+    if round(diff % 10.00) == 0 or current == total:
+        percentage = current * 100 / total
+        speed = current / diff
+        time_to_completion = round((total - current) / speed) * 1000
+        progress_str = "`[{0}{1}] {2}%`\n\n".format(
+            "".join("●" for i in range(math.floor(percentage / 5))),
+            "".join("" for i in range(20 - math.floor(percentage / 5))),
+            round(percentage, 2),
+        )
+
+        tmp = (
+            progress_str
+            + "`{0} of {1}`\n\n`✦ Speed: {2}/s`\n\n`✦ ETA: {3}`\n\n".format(
+                humanbytes(current),
+                humanbytes(total),
+                humanbytes(speed),
+                time_formatter(time_to_completion),
+            )
+        )
+        if file_name:
+            await event.edit(
+                "`✦ {}`\n\n`File Name: {}`\n\n{}".format(type_of_ps, file_name, tmp)
+            )
+        else:
+            await event.edit("`✦ {}`\n\n{}".format(type_of_ps, tmp))
+
+
+# ------------------System\\Heroku stuff----------------
+# @xditya @sppidy @techierror
+
+
+async def restart(ult):
+    if Var.HEROKU_APP_NAME and Var.HEROKU_API:
+        try:
+            Heroku = heroku3.from_key(Var.HEROKU_API)
+            app = Heroku.apps()[Var.HEROKU_APP_NAME]
+            await ult.edit("`Restarting your app, please wait for a minute!`")
+            app.restart()
+        except BaseException:
+            return await eor(
+                ult,
+                "`HEROKU_API` or `HEROKU_APP_NAME` is wrong! Kindly re-check in config vars.",
+            )
+    else:
+        execl(executable, executable, "-m", "pyUltroid")
+
+
+async def shutdown(ult, dynotype="ultroid"):
+    ult = await eor(ult, "Shutting Down")
+    if Var.HEROKU_APP_NAME and Var.HEROKU_API:
+        try:
+            Heroku = heroku3.from_key(Var.HEROKU_API)
+            app = Heroku.apps()[Var.HEROKU_APP_NAME]
+            await ult.edit("`Shutting Down your app, please wait for a minute!`")
+            app.process_formation()[dynotype].scale(0)
+        except BaseException as e:
+            LOGS.info(e)
+            return await ult.edit(
+                "`HEROKU_API` and `HEROKU_APP_NAME` is wrong! Kindly re-check in config vars."
+            )
+    else:
+        exit(1)
+
+
+async def heroku_logs(event):
+    xx = await eor(event, "`Processing...`")
+    if not (Var.HEROKU_API and Var.HEROKU_APP_NAME):
+        return await xx.edit("Please set `HEROKU_APP_NAME` and `HEROKU_API` in vars.")
+    try:
+        app = (heroku3.from_key(Var.HEROKU_API)).app(Var.HEROKU_APP_NAME)
+    except BaseException as se:
+        LOGS.info(se)
+        return await xx.edit(
+            "`HEROKU_API` and `HEROKU_APP_NAME` is wrong! Kindly re-check in config vars."
+        )
+    await xx.edit("`Downloading Logs...`")
+    ok = app.get_log()
+    with open("ultroid-heroku.log", "w") as log:
+        log.write(ok)
+    await event.client.send_file(
+        event.chat_id,
+        file="ultroid-heroku.log",
+        thumb="resources/extras/ultroid.jpg",
+        caption="**Ultroid Heroku Logs.**",
+    )
+
+    os.remove("ultroid-heroku.log")
+    await xx.delete()
+
+
+async def def_logs(ult):
+    await ult.client.send_file(
+        ult.chat_id,
+        file="ultroid.log",
+        thumb="resources/extras/ultroid.jpg",
+        caption="**Ultroid Logs.**",
+    )
+
