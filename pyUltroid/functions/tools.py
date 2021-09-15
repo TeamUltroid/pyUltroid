bc7f8da2dcea706d798e6d24126781ae00aab937
TeamUltroid <teamultroid@ultroid.tech>
2021-09-15 09:32:31 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/tools.py
+++ pyUltroid/functions/tools.py
@@ -4,18 +4,20 @@ import ssl
 import subprocess
 import sys
 import traceback
+
 import aiohttp
 import requests
 from PIL import Image, ImageDraw, ImageFont
 
 from . import LOGS, ultroid_bot
 from .helper import fast_download, json_parser
+
 try:
-   import cv2
+    import cv2
 except ImportError:
     cv2 = None
 try:
-   import numpy as np
+    import numpy as np
 except ImportError:
     np = None
 # ~~~~~~~~~~~~~~~~~~~~OFOX API~~~~~~~~~~~~~~~~~~~~
@@ -313,20 +315,22 @@ def stdr(seconds):
     )
 
 
-#------------------- used in pdftools --------------------#
+# ------------------- used in pdftools --------------------#
 
-#https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example
+# https://www.pyimagesearch.com/2014/08/25/4-point-opencv-getperspective-transform-example
+
 
 def order_points(pts):
     "get the required points"
-    rect = np.zeros((4, 2), dtype = "float32")
-    s = pts.sum(axis = 1)
+    rect = np.zeros((4, 2), dtype="float32")
+    s = pts.sum(axis=1)
     rect[0] = pts[np.argmin(s)]
     rect[2] = pts[np.argmax(s)]
-    diff = np.diff(pts, axis = 1)
+    diff = np.diff(pts, axis=1)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
     return rect
+
 
 def four_point_transform(image, pts):
     rect = order_points(pts)
@@ -337,13 +341,13 @@ def four_point_transform(image, pts):
     heightA = np.sqrt(((tr[0] - br[0]) ** 2) + ((tr[1] - br[1]) ** 2))
     heightB = np.sqrt(((tl[0] - bl[0]) ** 2) + ((tl[1] - bl[1]) ** 2))
     maxHeight = max(int(heightA), int(heightB))
-    dst = np.array([
-        [0, 0],
-        [maxWidth - 1, 0],
-        [maxWidth - 1, maxHeight - 1],
-        [0, maxHeight - 1]], dtype="float32")
+    dst = np.array(
+        [[0, 0], [maxWidth - 1, 0], [maxWidth - 1, maxHeight - 1], [0, maxHeight - 1]],
+        dtype="float32",
+    )
     M = cv2.getPerspectiveTransform(rect, dst)
     warped = cv2.warpPerspective(image, M, (maxWidth, maxHeight))
     return warped
+
 
 # --------- END --------- #
