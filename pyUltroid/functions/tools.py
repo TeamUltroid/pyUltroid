d600afea4af3c66c82d05190f42288954809b676
Devesh Pal <newdev0@outlook.com>
2021-09-15 11:04:45 +0530

    Update tools.py

--- pyUltroid/functions/tools.py
+++ pyUltroid/functions/tools.py
@@ -215,3 +215,14 @@ async def resize_photo(photo):
         maxsize = (512, 512)
         image.thumbnail(maxsize)
     return image
+
+def check_filename(filroid):
+    if os.path.exists(filroid):
+        no = 1
+        while True:
+            ult = "{0}_{2}{1}".format(*os.path.splitext(filroid) + (no,))
+            if os.path.exists(ult):
+                no += 1
+            else:
+                return ult
+    return filroid
