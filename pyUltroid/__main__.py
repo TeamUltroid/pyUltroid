4ec8c6355232f984f29bd3f6908b343548d6ad95
Devesh Pal <newdev0@outlook.com>
2021-09-16 09:04:45 +0530

    Update __main__.py

--- pyUltroid/__main__.py
+++ pyUltroid/__main__.py
@@ -8,13 +8,13 @@
 
 from .startup.BaseClient import UltroidClient
 from .startup.connections import RedisConnection, session_file
-
+from .dB._database import Var
 udB = RedisConnection(host="", port=None, password="", platform=where_hosted())
 
 ultroid_bot = UltroidClient(
-    session_file(), api_id=None, api_hash="", plugins_path="plugins"
+    session_file(), api_id=Var.API_ID, api_hash=Var.API_HASH, plugins_path="plugins"
 )
-asst = UltroidClient("Needed?", api_id=None, api_hash="", bot_token="")
+asst = UltroidClient(None, api_id=None, api_hash="", bot_token=Var.BOT_TOKEN)
 
 # Some Automations
 
