9e0508c1095fa041458b52d9af59c5de93ba25d8
TeamUltroid <teamultroid@ultroid.tech>
2021-09-16 03:27:18 +0000

    pyLint: auto-fixes

--- pyUltroid/__main__.py
+++ pyUltroid/__main__.py
@@ -6,12 +6,14 @@
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 
-from .startup.connections import RedisConnection
 from .startup.BaseClient import UltroidClient
+from .startup.connections import RedisConnection
 
 udB = RedisConnection(host="", port=None, password="", platform=where_hosted())
 
-ultroid_bot = UltroidClient(get_session(), api_id=None, api_hash="", plugins_path="plugins")
+ultroid_bot = UltroidClient(
+    get_session(), api_id=None, api_hash="", plugins_path="plugins"
+)
 asst = UltroidClient("Needed?", api_id=None, api_hash="", bot_token="")
 
 # Some Automations
