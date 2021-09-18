c7c7ab1ca025c42e71b218de7091033f038663e0
Devesh Pal <newdev0@outlook.com>
2021-09-18 18:40:40 +0500

    hurr

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -6,16 +6,17 @@
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 import os
-
-from .configs import Var
-from logging import INFO, FileHandler, StreamHandler, basicConfig, getLogger
 from .startup.connections import RedisConnection, session_file, where_hosted, vc_connection
 from .startup.funcs import autobot
 from .startup.BaseClient import UltroidClient
+from .configs import Var
+from logging import INFO, FileHandler, StreamHandler, basicConfig, getLogger
+
 from telethon import __version__
 
 from .version import __version__ as __pyUltroid__
 from .version import ultroid_version
+
 if os.path.exists("ultroid.log"):
     os.remove("ultroid.log")
 
@@ -43,8 +44,7 @@ LOGS.info(f"Telethon Version - {__version__}")
 LOGS.info(f"Ultroid Version - {ultroid_version}")
 
 
-udB = RedisConnection(host=Var.REDIS_URI, port=None, password=Var.REDIS_PASSWORD, platform=HOSTED_ON, logger=LOGS,
-socket_timeout=5)
+udB = RedisConnection(host=Var.REDIS_URI, port=None, password=Var.REDIS_PASSWORD, platform=HOSTED_ON)
 
 ultroid_bot = UltroidClient(
     session_file(),
@@ -66,7 +66,7 @@ asst = UltroidClient(
 asst.me = ultroid_bot.run_in_loop(asst.get_me())
 ultroid_bot.me = ultroid_bot.run_in_loop(ultroid_bot.get_me())
 
-vcClient = vc_connection(udB, ultroid_bot, LOGS)
+vcClient = vc_connection(udB, ultroid_bot)
 
 if not udB.get("SUDO"):
     udB.set("SUDO", "False")
---
+++ pyUltroid/dB/setup.py
@@ -0,0 +1,50 @@
+import re
+
+import setuptools
+
+with open("pyUltroid/version.py", "rt", encoding="utf8") as x:
+    version = re.search(r'__version__ = "(.*?)"', x.read()).group(1)
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+name = "py-Ultroid"
+author = "TeamUltroid"
+author_email = "teamultroid@protonmail.ch"
+description = "A Secure and Powerful Python-Telethon Based Library For Ultroid Userbot."
+license = "GNU AFFERO GENERAL PUBLIC LICENSE (v3)"
+url = "https://github.com/TeamUltroid/pyUltroid"
+project_urls = {
+    "Bug Tracker": "https://github.com/TeamUltroid/pyUltroid/issues",
+    "Documentation": "https://ultroid.tech",
+    "Source Code": "https://github.com/TeamUltroid/pyUltroid",
+}
+classifiers = [
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Operating System :: OS Independent",
+]
+
+with open("requirements.txt", "r") as file:
+    requirements = file.read().split("\n")
+
+setuptools.setup(
+    name=name,
+    version=version,
+    author=author,
+    author_email=author_email,
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=url,
+    project_urls=project_urls,
+    license=license,
+    packages=setuptools.find_packages(),
+    install_requires=requirements,
+    classifiers=classifiers,
+    python_requires=">=3.6",
+)
