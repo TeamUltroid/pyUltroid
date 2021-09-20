8a1ad168de3c9b89af1bc45ded8288b76f95eb8d
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-20 10:55:02 +0530

    Update utils.py

--- pyUltroid/startup/utils.py
+++ pyUltroid/startup/utils.py
@@ -74,80 +74,78 @@ def load_plugins(plugin_name):
 
 
 def load_addons(plugin_name):
-    if plugin_name.startswith("__"):
-        return
-    from .. import HNDLR, LOGS, asst, udB, ultroid_bot
-    from ..configs import Var
-    from ..dB._core import ADDONS, HELP
-    from ..misc import _supporter as xxx
-    from ..misc._assistant import (
+    if not plugin_name.startswith("__"):
+        from .. import HNDLR, LOGS, asst, udB, ultroid_bot
+        from ..configs import Var
+        from ..dB._core import ADDONS, HELP
+        from ..misc import _supporter as xxx
+        from ..misc._assistant import (
         asst_cmd,
         callback,
         in_pattern,
         inline,
         inline_owner,
         owner,
-    )
-    from ..misc._decorators import ultroid_cmd
-    from ..misc._supporter import Config, admin_cmd, sudo_cmd
-    from ..misc._wrappers import eod, eor
+        )
+        from ..misc._decorators import ultroid_cmd
+        from ..misc._supporter import Config, admin_cmd, sudo_cmd
+        from ..misc._wrappers import eod, eor
 
-    path = Path(f"addons/{plugin_name}.py")
-    name = "addons.{}".format(plugin_name)
-    spec = util.spec_from_file_location(name, path)
-    mod = util.module_from_spec(spec)
-    mod.asst = asst
-    mod.tgbot = asst
-    mod.ultroid_bot = ultroid_bot
-    mod.ub = ultroid_bot
-    mod.bot = ultroid_bot
-    mod.ultroid = ultroid_bot
-    mod.borg = ultroid_bot
-    mod.telebot = ultroid_bot
-    mod.jarvis = ultroid_bot
-    mod.friday = ultroid_bot
-    mod.owner = owner()
-    mod.in_owner = inline_owner()
-    mod.inline = inline()
-    mod.eod = eod
-    mod.edit_delete = eod
-    mod.LOGS = LOGS
-    mod.in_pattern = in_pattern
-    mod.hndlr = HNDLR
-    mod.handler = HNDLR
-    mod.HNDLR = HNDLR
-    mod.CMD_HNDLR = HNDLR
-    mod.Config = Config
-    mod.Var = Var
-    mod.eor = eor
-    mod.edit_or_reply = eor
-    mod.asst_cmd = asst_cmd
-    mod.ultroid_cmd = ultroid_cmd
-    mod.on_cmd = ultroid_cmd
-    mod.callback = callback
-    mod.Redis = udB.get
-    mod.admin_cmd = admin_cmd
-    mod.sudo_cmd = sudo_cmd
-    modules["ub"] = xxx
-    modules["var"] = xxx
-    modules["jarvis"] = xxx
-    modules["support"] = xxx
-    modules["userbot"] = xxx
-    modules["telebot"] = xxx
-    modules["fridaybot"] = xxx
-    modules["jarvis.utils"] = xxx
-    modules["uniborg.util"] = xxx
-    modules["telebot.utils"] = xxx
-    modules["userbot.utils"] = xxx
-    modules["userbot.events"] = xxx
-    modules["jarvis.jconfig"] = xxx
-    modules["userbot.config"] = xxx
-    modules["fridaybot.utils"] = xxx
-    modules["fridaybot.Config"] = xxx
-    modules["userbot.uniborgConfig"] = xxx
-    spec.loader.exec_module(mod)
-    modules["addons." + plugin_name] = mod
-    if not plugin_name.startswith("_"):
+        path = "addons/" + plugin_name
+        name = path.replace("/", ".")[:-3]
+        spec = util.spec_from_file_location(name, path)
+        mod = util.module_from_spec(spec)
+        mod.asst = asst
+        mod.tgbot = asst
+        mod.ultroid_bot = ultroid_bot
+        mod.ub = ultroid_bot
+        mod.bot = ultroid_bot
+        mod.ultroid = ultroid_bot
+        mod.borg = ultroid_bot
+        mod.telebot = ultroid_bot
+        mod.jarvis = ultroid_bot
+        mod.friday = ultroid_bot
+        mod.owner = owner()
+        mod.in_owner = inline_owner()
+        mod.inline = inline()
+        mod.eod = eod
+        mod.edit_delete = eod
+        mod.LOGS = LOGS
+        mod.in_pattern = in_pattern
+        mod.hndlr = HNDLR
+        mod.handler = HNDLR
+        mod.HNDLR = HNDLR
+        mod.CMD_HNDLR = HNDLR
+        mod.Config = Config
+        mod.Var = Var
+        mod.eor = eor
+        mod.edit_or_reply = eor
+        mod.asst_cmd = asst_cmd
+        mod.ultroid_cmd = ultroid_cmd
+        mod.on_cmd = ultroid_cmd
+        mod.callback = callback
+        mod.Redis = udB.get
+        mod.admin_cmd = admin_cmd
+        mod.sudo_cmd = sudo_cmd
+        modules["ub"] = xxx
+        modules["var"] = xxx
+        modules["jarvis"] = xxx
+        modules["support"] = xxx
+        modules["userbot"] = xxx
+        modules["telebot"] = xxx
+        modules["fridaybot"] = xxx
+        modules["jarvis.utils"] = xxx
+        modules["uniborg.util"] = xxx
+        modules["telebot.utils"] = xxx
+        modules["userbot.utils"] = xxx
+        modules["userbot.events"] = xxx
+        modules["jarvis.jconfig"] = xxx
+        modules["userbot.config"] = xxx
+        modules["fridaybot.utils"] = xxx
+        modules["fridaybot.Config"] = xxx
+        modules["userbot.uniborgConfig"] = xxx
+        spec.loader.exec_module(mod)
+        modules["addons." + plugin_name] = mod
         if plugin_name not in ADDONS:
             ADDONS.append(plugin_name)
         try:
