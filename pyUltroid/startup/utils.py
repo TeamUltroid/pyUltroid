177b0c97b5762ca346b4f4f2a0b85b2e661ac56e
TeamUltroid <teamultroid@ultroid.tech>
2021-09-22 17:16:28 +0000

    pyLint: auto-fixes

--- pyUltroid/startup/utils.py
+++ pyUltroid/startup/utils.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
-from importlib import util, import_module
+from importlib import import_module, util
 from pathlib import Path
 from sys import modules
 
