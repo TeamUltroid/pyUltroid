108fa05a04bfe271edb980549238a2fd4b3c9376
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-24 12:53:15 +0530

    Update loader.py

--- pyUltroid/startup/loader.py
+++ pyUltroid/startup/loader.py
@@ -40,9 +40,7 @@ class Loader:
             except Exception as exc:
                 self._logger.info(f"Ultroid - {self.key} - ERROR - {plugin}")
                 self._logger.exception(exc)
-            if cmd_help or cmd_help == {} and doc.__doc__:
-                if plugin.startswith("_"):
-                    return
+            if (cmd_help or cmd_help == {}) and not plugin.startswith("_") and doc.__doc__:
                 doc = doc.__doc__.format(i=HNDLR)
                 if self.key in cmd_help.keys():
                     update_cmd = cmd_help[self.key]
