cfeed347697395ffce9f34f0cd5e7f335b0a5486
Devesh Pal <newdev0@outlook.com>
2021-09-24 21:37:25 +0530

    Update __main__.py

--- pyUltroid/__main__.py
+++ pyUltroid/__main__.py
@@ -10,12 +10,12 @@ from . import *
 from .startup.funcs import autopilot, customize, plug, ready, startup_stuff
 from .startup.loader import load_other_plugins
 
-"""
+
 # Option to Auto Update On Restarts..
 if udB.get("UPDATE_ON_RESTART") and os.path.exists(".git") and updater():
     os.system("git pull -f && pip3 install --no-cache-dir -r requirements.txt")
     os.execl(sys.executable, "python3", "-m", "pyUltroid")
-"""
+
 startup_stuff()
 
 
@@ -38,7 +38,7 @@ addons = udB.get("ADDONS") or Var.ADDONS
 vcbot = udB.get("VCBOT") or Var.VCBOT
 
 # Railway dont allow Music Bots
-if HOSTED_ON == "railway" and not udB.get("VCBOT"):
+if HOSTED_ON == "railway" and vcbot:
     vcbot = "False"
 
 load_other_plugins(addons=addons, pmbot=pmbot, manager=manager, vcbot=vcbot, udB=udB)
