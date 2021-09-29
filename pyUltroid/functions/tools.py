dbd47567efc9955137f2b0c944945f32cdea3651
Devesh Pal <newdev0@outlook.com>
2021-09-29 20:16:51 +0530

    Thats why main branch exists

--- pyUltroid/functions/tools.py
+++ pyUltroid/functions/tools.py
@@ -242,6 +242,20 @@ def get_all_files(path):
             filelist.append(os.path.join(root, file))
     return sorted(filelist)
 
+def text_set(text):
+    lines = []
+    if len(text) <= 55:
+        lines.append(text)
+    else:
+        all_lines = text.split("\n")
+        for line in all_lines:
+            if len(line) <= 55:
+                lines.append(line)
+            else:
+                k = int(len(line) / 55)
+                for z in range(1, k + 2):
+                    lines.append(line[((z - 1) * 55) : (z * 55)])
+    return lines[:25]
 
 # ------------------Logo Gen Helpers----------------
 # Add ur usernames who created
