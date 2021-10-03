47fb758433da48eb5e2f0aaf4ef3a99da0b8f9a6
TeamUltroid <teamultroid@ultroid.tech>
2021-10-03 08:18:21 +0000

    pyLint: auto-fixes

--- pyUltroid/startup/loader.py
+++ pyUltroid/startup/loader.py
@@ -68,7 +68,9 @@ def load_other_plugins(addons=None, pmbot=None, manager=None, vcbot=None):
     Loader(path="plugins", key="Official", logger=LOGS).load()
 
     # for assistant
-    Loader(path="assistant", key="Assistant", logger=LOGS).load(log=False, cmd_help=None)
+    Loader(path="assistant", key="Assistant", logger=LOGS).load(
+        log=False, cmd_help=None
+    )
 
     # for addons
     if addons == "True" or not addons:
