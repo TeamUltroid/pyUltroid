520a1bb0956a9295a0a0f3b86fd5e238a2b9c142
Devesh Pal <newdev0@outlook.com>
2021-10-07 01:57:21 +0530

    Update notes_db.py

--- pyUltroid/dB/notes_db.py
+++ pyUltroid/dB/notes_db.py
@@ -7,14 +7,19 @@
 
 from .. import udB
 
-try:
-    eval(udB["NOTE"])
-except BaseException:
-    udB.set("NOTE", "{}")
+def get_stuff():
+    a = udB.get("NOTE")
+    if not a:
+        return {}
+    try:
+        return eval(a)
+    except BaseException:
+        udB.delete("NOTE")
+    return {}
 
 
 def add_note(chat, word, msg, media, button):
-    ok = eval(udB.get("NOTE"))
+    ok = get_stuff()
     if ok.get(chat):
         ok[chat].update({word: {"msg": msg, "media": media, "button": button}})
     else:
@@ -23,28 +28,28 @@ def add_note(chat, word, msg, media, button):
 
 
 def rem_note(chat, word):
-    ok = eval(udB.get("NOTE"))
+    ok = get_stuff()
     if ok.get(chat) and ok[chat].get(word):
         ok[chat].pop(word)
         udB.set("NOTE", str(ok))
 
 
 def rem_all_note(chat):
-    ok = eval(udB.get("NOTE"))
+    ok = get_stuff()
     if ok.get(chat):
         ok.pop(chat)
         udB.set("NOTE", str(ok))
 
 
 def get_notes(chat, word):
-    ok = eval(udB.get("NOTE"))
+    ok = get_stuff()
     if ok.get(chat) and ok[chat].get(word):
         return ok[chat][word]
     return False
 
 
 def list_note(chat):
-    ok = eval(udB.get("NOTE"))
+    ok = get_stuff()
     if ok.get(chat):
         return "".join(f"👉 #{z}\n" for z in ok[chat])
     return False
