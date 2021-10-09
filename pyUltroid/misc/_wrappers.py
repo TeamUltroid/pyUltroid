813590f48978928d4d8e7e1c0747d0105f237e10
Devesh Pal <newdev0@outlook.com>
2021-10-09 18:42:53 +0530

    Update _wrappers.py

--- pyUltroid/misc/_wrappers.py
+++ pyUltroid/misc/_wrappers.py
@@ -12,17 +12,21 @@ import asyncio
 DEL_TIME = []
 
 
-async def eor(event, text, **kwargs):
-    time = kwargs.get("time", None)
-    kwargs["message"] = text
-    if "time" in kwargs:
-        del kwargs["time"]
-    del kwargs["text"]
+async def eor(event, text, **args):
+    link_preview = args.get("link_preview", False)
+    parse_mode = args.get("parse_mode", "md")
+    time = args.get("time", None)
     if not event.out:
-        kwargs["reply_to"] = event.reply_to_msg_id or event
-        ok = await event.client.send_message(event.chat_id, **kwargs)
+        reply_to = event.reply_to_msg_id or event
+        ok = await event.client.send_message(
+            event.chat_id,
+            text,
+            link_preview=link_preview,
+            parse_mode=parse_mode,
+            reply_to=reply_to,
+        )
     else:
-        ok = await event.edit(**kwargs)
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
     if not DEL_TIME:
         from .. import udB
 
