1dde5411ce86e43b1c85d821325c7e041da143a1
TeamUltroid <teamultroid@ultroid.tech>
2021-10-09 12:28:35 +0000

    pyLint: auto-fixes

--- pyUltroid/misc/_wrappers.py
+++ pyUltroid/misc/_wrappers.py
@@ -13,17 +13,14 @@ DEL_TIME = []
 
 
 async def eor(event, **kwargs):
-    link_preview = kwargs.get("link_preview", False)
-    parse_mode = kwargs.get("parse_mode", "md")
+    kwargs.get("link_preview", False)
+    kwargs.get("parse_mode", "md")
     time = kwargs.get("time", None)
     if "time" in kwargs:
         del kwargs["time"]
     if not event.out:
         kwargs["reply_to"] = event.reply_to_msg_id or event
-        ok = await event.client.send_message(
-            event.chat_id,
-            **kwargs
-        )
+        ok = await event.client.send_message(event.chat_id, **kwargs)
     else:
         ok = await event.edit(**kwargs)
     if not DEL_TIME:
