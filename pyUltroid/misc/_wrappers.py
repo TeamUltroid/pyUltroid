ad545f09f9b435d3368b0d2e63c7eff8bd3219d2
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-09 15:02:50 +0530

    Update _wrappers.py

--- pyUltroid/misc/_wrappers.py
+++ pyUltroid/misc/_wrappers.py
@@ -9,34 +9,29 @@ import asyncio
 
 # edit or reply
 
-DEL_TIME = []
+DEL_TIME = 0
 
 
-async def eor(event, text, **args):
-    link_preview = args.get("link_preview", False)
-    parse_mode = args.get("parse_mode", "md")
-    time = args.get("time", None)
+async def eor(event, text, **kwargs):
+    from .. import udB
+    if DEL_TIME == 0:
+        DEL_TIME += int(udB.get("DEL_DELAY_TIME")) or 0
+    kwargs["link_preview"] = kwargs.get("link_preview", False)
+    kwargs["parse_mode"] = kwargs.get("parse_mode", "md")
+    time = kwargs.get("time", DEL_TIME)
+    if "time" in kwargs:
+        del kwargs["time"]
     if not event.out:
-        reply_to = event.reply_to_msg_id or event
+        kwargs["reply_to"] = event.reply_to_msg_id or event
         ok = await event.client.send_message(
             event.chat_id,
             text,
-            link_preview=link_preview,
-            parse_mode=parse_mode,
-            reply_to=reply_to,
+            **kwargs,
         )
     else:
         ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    if not DEL_TIME:
-        from .. import udB
-
-        ut = udB.get("DEL_DELAY_TIME")
-        DEL_TIME.append(ut)
-    else:
-        ut = DEL_TIME[0]
-    if time and ut != "None":
-        time = ut or time
-        await asyncio.sleep(int(time))
+    if time and time != 0:
+        await asyncio.sleep(time)
         return await ok.delete()
     return ok
 
