aa5c30b713e8ff55282b4d51a1e6accefaf79aa4
TechiError <error@notavailable.live>
2021-10-14 21:33:47 +0530

    Some functions

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -1,3 +1,5 @@
+from . import *
+
 from pydrive2.auth import GoogleAuth
 from pydrive2.drive import GoogleDrive
 
@@ -12,3 +14,29 @@ class GDriveManager:
 
     def _authorise(self, token: str):
         self.GoogleAuth.Auth(token)
+
+	def _save_credentials_file(self, path_to_file: str = "./resources/auth/client_secrets.json"):
+		self.SaveCredentialsFile(path_to_file)
+	
+	def _login(self, path_to_file: str = "./resources/auth/client_secrets.json"):
+		try:
+			self.LoadCredentialsFile(path_to_file)
+			return "Success"
+		except Exception as e:
+			print(e)
+			return "Error"
+	
+	def _initialize_cs_json(self, path_to_file: str = "./resources/auth/client_secrets.json"):
+		_json = {
+			"installed":{
+				"client_id":udB.get("GDRIVE_CLIENT_ID"),
+				"client_secret":udB.get("GDRIVE_CLIENT_SECRET"),
+				"auth_uri":"https://accounts.google.com/o/oauth2/auth",
+				"token_uri":"https://oauth2.googleapis.com/token",
+				"redirect_uris": ["urn:ietf:wg:oauth:2.0:oob"],
+				"save_credentials": True,
+				"access_type": "offline"
+			}
+		}
+		with open(path_to_file, "w") as f:
+			f.write(str(_json).replace("\'", "\"").replace("True", "true"))
