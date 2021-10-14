603378fdf6da33f0b07281732b6b1b5b2c214084
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-14 21:02:24 +0530

    Create gDrive.py

---
+++ pyUltroid/functions/gDrive.py
@@ -0,0 +1,16 @@
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+from .. import udB
+
+
+class GDriveManager:
+    def __init__(self):
+        self.GoogleAuth = GoogleAuth()
+        self.GoogleDrive = GoogleDrive(self.GoogleAuth)
+
+    def _generate_auth_url(self):
+        return self.GoogleAuth.GetAuthUrl()
+
+    def _authorise(self, token: str):
+        self.GoogleAuth.Auth(token)
+        
