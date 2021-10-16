e2ff2a4881637c7f7fe72abede47fce1a5c33d44
TeamUltroid <teamultroid@ultroid.tech>
2021-10-16 18:52:56 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -18,8 +18,13 @@ from .helper import humanbytes, time_formatter
 
 class GDriveManager:
     def __init__(self):
-        self.gdrive_creds = {"oauth_scope": "https://www.googleapis.com/auth/drive", "mimetype": "application/vnd.google-apps.folder", "auth_uri": "https://accounts.google.com/o/oauth2/auth", "token_uri": "https://oauth2.googleapis.com/token", "redirect_uris": "urn:ietf:wg:oauth:2.0:oob"}
+        self.gdrive_creds = {
+            "oauth_scope": "https://www.googleapis.com/auth/drive",
+            "mimetype": "application/vnd.google-apps.folder",
+            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+            "token_uri": "https://oauth2.googleapis.com/token",
+            "redirect_uris": "urn:ietf:wg:oauth:2.0:oob",
+        }
 
     def _authorise(self):
-        something #todo
-        
+        something  # todo
