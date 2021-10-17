2f050c889c971a59078c484b3731a7cef90305e9
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-17 23:40:50 +0530

    Chlti chiz ko khodna koi inse sikhe

--- pyUltroid/functions/helper.py
+++ pyUltroid/functions/helper.py
@@ -396,7 +396,7 @@ No_Flood = {}
 
 
 async def progress(current, total, event, start, type_of_ps, file_name=None):
-    now = time.time()
+    """now = time.time()
     if No_Flood.get(event.chat_id):
         if No_Flood[event.chat_id].get(e.id):
             if (now - No_Flood[event.chat_id][e.id]) < 1.1:
@@ -404,8 +404,8 @@ async def progress(current, total, event, start, type_of_ps, file_name=None):
         else:
             No_Flood[event.chat_id].update({e.id: now})
     else:
-        No_Flood.update({event.chat_id: {e.id: now}})
-    diff = now - start
+        No_Flood.update({event.chat_id: {e.id: now}})"""
+    diff = time.time() - start
     if round(diff % 10.00) == 0 or current == total:
         percentage = current * 100 / total
         speed = current / diff
