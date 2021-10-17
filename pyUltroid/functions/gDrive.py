f3823c91cd6bf5a086375c060453ff6c08043890
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-17 13:12:31 +0530

    Update gDrive.py

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -7,19 +7,20 @@ from mimetypes import guess_type
 import httplib2
 from apiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
-from oauth2client.file import Storage
 from telethon import events
 
 from .. import *
 from .helper import humanbytes, time_formatter
 """
 
+from oauth2client.file import Storage
 from oauth2client.client import OAuth2WebServerFlow, FlowExchangeError
 from .. import udB
 
 
 class GDriveManager:
     def __init__(self):
+        self.flow = None
         self.gdrive_creds = {
             "oauth_scope": ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive.metadata"],
             "mimetype": "application/vnd.google-apps.folder",
@@ -27,23 +28,14 @@ class GDriveManager:
         }
         self.auth_token = udB.get("GDRIVE_AUTH_TOKEN")
 
-    def _authorise(self):
-        self.flow = None
-        if not self.flow:
-            try:
-                self.flow = OAuth2WebServerFlow(udB["GDRIVE_CLIENT_ID"], udB["GDRIVE_CLIENT_SECRET"], self.gdrive_creds["oauth_scope"], redirect_uri=self.gdrive_creds["redirect_uri"])
-            except KeyError:
-                some other function
-            return self.flow.step1_get_authorize_url()
-        else:
-            return None
-
-    def _verify_auth(self, token: str):
-        _verified = None
-        if self.flow:
-            try:
-                return self.flow.step2_exchange(token)
-            except FlowExchangeError as e:
-                return str(e)
-        else:
-            self._authorise()
+    async def _create_token_file(self, token_file: str = "resources/auth/gdrive_creds.json", code: str = None):
+        if code and self.flow:
+            self.flow.step2_exchange(code)
+            storage = Storage(token_file)
+            storage.put(credentials)
+            return storage
+        try:
+            self.flow = OAuth2WebServerFlow(udB["GDRIVE_CLIENT_ID"], udB["GDRIVE_CLIENT_SECRET"], self.gdrive_creds["oauth_scope"], redirect_uri=self.gdrive_creds["redirect_uri"])
+        except KeyError:
+            return "Fill GDRIVE client credentials"
+        return self.flow.step1_get_authorize_url()
