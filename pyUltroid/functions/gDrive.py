13724f0ccd186527407fec080da7862e4140b815
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-17 13:02:09 +0530

    Update gDrive.py

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -7,7 +7,6 @@ from mimetypes import guess_type
 import httplib2
 from apiclient.http import MediaFileUpload
 from googleapiclient.discovery import build
-from oauth2client.client import OAuth2WebServerFlow
 from oauth2client.file import Storage
 from telethon import events
 
@@ -15,16 +14,36 @@ from .. import *
 from .helper import humanbytes, time_formatter
 """
 
+from oauth2client.client import OAuth2WebServerFlow, FlowExchangeError
+from .. import udB
+
 
 class GDriveManager:
     def __init__(self):
         self.gdrive_creds = {
-            "oauth_scope": "https://www.googleapis.com/auth/drive",
+            "oauth_scope": ["https://www.googleapis.com/auth/drive","https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive.metadata"],
             "mimetype": "application/vnd.google-apps.folder",
-            "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-            "token_uri": "https://oauth2.googleapis.com/token",
-            "redirect_uris": "urn:ietf:wg:oauth:2.0:oob",
+            "redirect_uri": "urn:ietf:wg:oauth:2.0:oob",
         }
+        self.auth_token = udB.get("GDRIVE_AUTH_TOKEN")
 
     def _authorise(self):
-        something  # todo
+        self.flow = None
+        if not self.flow:
+            try:
+                self.flow = OAuth2WebServerFlow(udB["GDRIVE_CLIENT_ID"], udB["GDRIVE_CLIENT_SECRET"], self.gdrive_creds["oauth_scope"], redirect_uri=self.gdrive_creds["redirect_uri"])
+            except KeyError:
+                some other function
+            return self.flow.step1_get_authorize_url()
+        else:
+            return None
+
+    def _verify_auth(self, token: str):
+        _verified = None
+        if self.flow:
+            try:
+                return self.flow.step2_exchange(token)
+            except FlowExchangeError as e:
+                return str(e)
+        else:
+            self._authorise()
