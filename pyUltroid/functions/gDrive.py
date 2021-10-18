6f5465bc4febed603d2137b33f492b206791c05b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-18 11:39:25 +0530

    Update gDrive.py

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -86,7 +86,7 @@ class GDriveManager:
             body["parents"] = [{"id": self.folder_id}]
         upload = (
             self.build.files()
-            .insert(body=body, media_body=media_body, supportsAllDrives=True))
+            .insert(body=body, media_body=media_body, supportsAllDrives=True)
         )
         _status = None
         while not _status:
