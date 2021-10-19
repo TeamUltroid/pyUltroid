5d95adbf1b6ff938da15a554e4da8c591ee80af7
Jacob black <88770407+nub-af@users.noreply.github.com>
2021-10-19 23:43:12 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -5,33 +5,29 @@
 # PLease read the GNU Affero General Public License in
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
-from decouple import config
-from dotenv import find_dotenv, load_dotenv
-
-load_dotenv(find_dotenv())
-
+from os import environ
 
 class Var:
     # mandatory
-    API_ID = config("API_ID", default=6, cast=int)
-    API_HASH = config("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
-    BOT_TOKEN = config("BOT_TOKEN", default=None)
-    SESSION = config("SESSION", default=None)
-    REDIS_URI = config("REDIS_URI", default=None)
-    REDIS_PASSWORD = config("REDIS_PASSWORD", default=None)
+    API_ID = environ.get("API_ID", default=6, cast=int)
+    API_HASH = environ.get("API_HASH", default="eb06d4abfb49dc3eeb1aeb98ae0f581e")
+    BOT_TOKEN = environ.get("BOT_TOKEN", default=None)
+    SESSION = environ.get("SESSION", default=None)
+    REDIS_URI = environ.get("REDIS_URI", default=None)
+    REDIS_PASSWORD = environ.get("REDIS_PASSWORD", default=None)
     # extras
-    LOG_CHANNEL = config("LOG_CHANNEL", default=0, cast=int)
-    HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
-    HEROKU_API = config("HEROKU_API", default=None)
-    VC_SESSION = config("VC_SESSION", default=None)
-    ADDONS = config("ADDONS", default="True")
-    VCBOT = config("VCBOT", default="True")
+    LOG_CHANNEL = envrion.get("LOG_CHANNEL", default=0, cast=int)
+    HEROKU_APP_NAME = environ.get("HEROKU_APP_NAME", default=None)
+    HEROKU_API = envrion.get("HEROKU_API", default=None)
+    VC_SESSION = envrion.get("VC_SESSION", default=None)
+    ADDONS = envrion.get("ADDONS", default="True")
+    VCBOT = envrion.get("VCBOT", default="True")
     # for railway
-    REDISPASSWORD = config("REDISPASSWORD", default=None)
-    REDISHOST = config("REDISHOST", default=None)
-    REDISPORT = config("REDISPORT", default=None)
-    REDISUSER = config("REDISUSER", default=None)
+    REDISPASSWORD = envrion.get("REDISPASSWORD", default=None)
+    REDISHOST = envrion.get("REDISHOST", default=None)
+    REDISPORT = envrion.get("REDISPORT", default=None)
+    REDISUSER = envrion.get("REDISUSER", default=None)
     # for deta base
-    DETA_KEY = config("DETA_KEY", default=None)
+    DETA_KEY = envrion.get("DETA_KEY", default=None)
     # for future
-    MULTI_SESSIONS = config("MULTI_SESSIONS", default=None)
+    MULTI_SESSIONS = envrion.get("MULTI_SESSIONS", default=None)
