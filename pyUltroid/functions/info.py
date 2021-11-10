beb481a77485958d2d06b84ba1437f7a7843fab6
Devesh Pal <newdev0@outlook.com>
2021-11-10 17:43:08 +0530

    Complete trash lol

--- pyUltroid/functions/info.py
+++ pyUltroid/functions/info.py
@@ -55,67 +55,6 @@ async def get_uinfo(e):
 # Random stuffs dk who added
 
 
-async def get_full_user(event):
-    if event.reply_to_msg_id:
-        previous_message = await event.get_reply_message()
-        if previous_message.forward:
-            replied_user = await event.client(
-                functions.users.GetFullUserRequest(
-                    previous_message.forward.from_id
-                    or previous_message.forward.channel_id
-                )
-            )
-        else:
-            replied_user = await event.client(
-                functions.users.GetFullUserRequest(previous_message.sender_id)
-            )
-        return replied_user, None
-    else:
-        input_str = None
-        try:
-            input_str = await get_user_id(event.pattern_match.group(1))
-        except IndexError as e:
-            return None, e
-        if event.message.entities is not None:
-            mention_entity = event.message.entities
-            probable_user_mention_entity = mention_entity[0]
-            if isinstance(probable_user_mention_entity, types.MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                replied_user = await event.client(
-                    functions.users.GetFullUserRequest(user_id)
-                )
-                return replied_user, None
-            else:
-                try:
-                    user_object = await event.client.get_entity(int(input_str))
-                    user_id = user_object.id
-                    replied_user = await event.client(
-                        functions.users.GetFullUserRequest(user_id)
-                    )
-                    return replied_user, None
-                except Exception as e:
-                    return None, e
-        elif event.is_private:
-            try:
-                user_id = event.chat_id
-                replied_user = await event.client(
-                    functions.users.GetFullUserRequest(user_id)
-                )
-                return replied_user, None
-            except Exception as e:
-                return None, e
-        else:
-            try:
-                user_object = await event.client.get_entity(int(input_str))
-                user_id = user_object.id
-                replied_user = await event.client(
-                    functions.users.GetFullUserRequest(user_id)
-                )
-                return replied_user, None
-            except Exception as e:
-                return None, e
-
-
 async def fetch_info(event):
     chat = event.pattern_match.group(1)
     if chat:
