cfe167a4667f46182761f1ce3ff5e73095b43a66
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-15 00:13:28 +0530

    Waah

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -24,8 +24,8 @@ udB = UltroidDB()
 if udB.ping():
     LOGS.info("Connected to Database Successfully!")
 
-BOT_MODE = udB.get("BOTMODE") == "True"
-DUAL_MODE = udB.get("DUAL_MODE") == "True"
+BOT_MODE = udB.get_key("BOTMODE") == "True"
+DUAL_MODE = udB.get_key("DUAL_MODE") == "True"
 
 if BOT_MODE:
     if DUAL_MODE:
@@ -38,34 +38,34 @@ else:
         udB=udB,
         app_version=ultroid_version,
         device_model="Ultroid",
-        proxy=udB.get("TG_PROXY"),
+        proxy=udB.get_key("TG_PROXY"),
     )
 
 if not BOT_MODE:
     ultroid_bot.run_in_loop(autobot())
 else:
-    if not udB.get("BOT_TOKEN") and Var.BOT_TOKEN:
-        udB.set("BOT_TOKEN", Var.BOT_TOKEN)
-    if not udB.get("BOT_TOKEN"):
+    if not udB.get_key("BOT_TOKEN") and Var.BOT_TOKEN:
+        udB.set_key("BOT_TOKEN", Var.BOT_TOKEN)
+    if not udB.get_key("BOT_TOKEN"):
         LOGS.info('"BOT_TOKEN" not Found! Please add it, in order to use "BOTMODE"')
         exit()
 
-asst = UltroidClient(None, bot_token=udB.get("BOT_TOKEN"), udB=udB)
+asst = UltroidClient(None, bot_token=udB.get_key("BOT_TOKEN"), udB=udB)
 
 if BOT_MODE:
     ultroid_bot = asst
 
 vcClient = vc_connection(udB, ultroid_bot)
 
-if not udB.get("SUDO"):
-    udB.set("SUDO", "False")
+if not udB.get_key("SUDO"):
+    udB.set_key("SUDO", "False")
 
-if udB.get("SUDOS") is None:
-    udB.set("SUDOS", "")
+if udB.get_key("SUDOS") is None:
+    udB.set_key("SUDOS", "")
 
-if not udB.get("BLACKLIST_CHATS"):
-    udB.set("BLACKLIST_CHATS", "[]")
+if not udB.get_key("BLACKLIST_CHATS"):
+    udB.set_key("BLACKLIST_CHATS", "[]")
 
-HNDLR = udB.get("HNDLR") or "."
-DUAL_HNDLR = udB.get("DUAL_HNDLR") or "/"
-SUDO_HNDLR = udB.get("SUDO_HNDLR") or HNDLR
+HNDLR = udB.get_key("HNDLR") or "."
+DUAL_HNDLR = udB.get_key("DUAL_HNDLR") or "/"
+SUDO_HNDLR = udB.get_key("SUDO_HNDLR") or HNDLR
