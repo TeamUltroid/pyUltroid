2458895158b177eced2a6f1fdd2180281d001106
Devesh Pal <newdev0@outlook.com>
2021-11-14 20:26:12 +0530

    Update _wrappers.py

--- pyUltroid/misc/_wrappers.py
+++ pyUltroid/misc/_wrappers.py
@@ -6,17 +6,15 @@
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 import asyncio
+from . import is_sudo
 
 # edit or reply
-
-DEL_TIME = []
-
 
 async def eor(event, text, **args):
     link_preview = args.get("link_preview", False)
     parse_mode = args.get("parse_mode", "md")
     time = args.get("time", None)
-    if not event.out:
+    if is_sudo(event.sender_id):
         reply_to = event.reply_to_msg_id or event
         ok = await event.client.send_message(
             event.chat_id,
@@ -27,16 +25,8 @@ async def eor(event, text, **args):
         )
     else:
         ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
-    if not DEL_TIME:
-        from .. import udB
-
-        ut = udB.get("DEL_DELAY_TIME")
-        DEL_TIME.append(ut)
-    else:
-        ut = DEL_TIME[0]
-    if time and ut != "None":
-        time = ut or time
-        await asyncio.sleep(int(time))
+    if time:
+        await asyncio.sleep(time)
         return await ok.delete()
     return ok
 
