3ea2b166613114191388f1874986a42d3193b15b
Devesh Pal <newdev0@outlook.com>
2021-11-14 23:36:49 +0530

    Update _wrappers.py

--- pyUltroid/misc/_wrappers.py
+++ pyUltroid/misc/_wrappers.py
@@ -5,18 +5,17 @@
 # PLease read the GNU Affero General Public License in
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
-import asyncio
-
-from . import is_sudo
+from asyncio import sleep
 
 # edit or reply
-
 
 async def eor(event, text, **args):
     link_preview = args.get("link_preview", False)
     parse_mode = args.get("parse_mode", "md")
     time = args.get("time", None)
-    if is_sudo(event.sender_id):
+    if event.out:
+        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+     else:
         reply_to = event.reply_to_msg_id or event
         ok = await event.client.send_message(
             event.chat_id,
@@ -25,14 +24,13 @@ async def eor(event, text, **args):
             parse_mode=parse_mode,
             reply_to=reply_to,
         )
-    else:
-        ok = await event.edit(text, link_preview=link_preview, parse_mode=parse_mode)
+
     if time:
-        await asyncio.sleep(time)
+        await sleep(time)
         return await ok.delete()
     return ok
 
 
 async def eod(event, text, **kwargs):
-    kwargs["time"] = kwargs.get("time", 5)
+    kwargs["time"] = kwargs.get("time", 8)
     return await eor(event, text, **kwargs)
