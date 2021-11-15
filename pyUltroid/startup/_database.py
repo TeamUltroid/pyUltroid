8a2d2fb99f0a78d0a2c64482b911ae339ffad608
Devesh Pal <newdev0@outlook.com>
2021-11-15 13:07:30 +0530

    Update _database.py

--- pyUltroid/startup/_database.py
+++ pyUltroid/startup/_database.py
@@ -31,7 +31,7 @@ if Deta and Var.DETA_KEY:
 
 def get_data(self_, key):
     data = self_.get(str(key))
-    if data and " " not in data:
+    if data:
         try:
             data = eval(data)
         except BaseException:
