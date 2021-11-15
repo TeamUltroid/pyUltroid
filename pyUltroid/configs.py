90750733b786e2eb32ca732808542d7459eb1895
Devesh Pal <newdev0@outlook.com>
2021-11-15 20:11:12 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -24,8 +24,8 @@ class Var:
     HEROKU_APP_NAME = config("HEROKU_APP_NAME", default=None)
     HEROKU_API = config("HEROKU_API", default=None)
     VC_SESSION = config("VC_SESSION", default=None)
-    ADDONS = config("ADDONS", default="True")
-    VCBOT = config("VCBOT", default="True")
+    ADDONS = config("ADDONS", default=False, cast=bool)
+    VCBOT = config("VCBOT", default=True, cast=bool)
     # for railway
     REDISPASSWORD = config("REDISPASSWORD", default=None)
     REDISHOST = config("REDISHOST", default=None)
