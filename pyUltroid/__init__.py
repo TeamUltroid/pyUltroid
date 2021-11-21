026b7964fb5fd26363366e8912d058fdd6c3af4c
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-11-21 22:07:03 +0530

    Update __init__.py

--- pyUltroid/__init__.py
+++ pyUltroid/__init__.py
@@ -26,6 +26,7 @@ if udB.ping():
 
 BOT_MODE = udB.get_key("BOTMODE")
 DUAL_MODE = udB.get_key("DUAL_MODE")
+have_brain = udB.get_key("I_HAVE_BRAIN") or Var.I_HAVE_BRAIN
 
 if BOT_MODE:
     if DUAL_MODE:
