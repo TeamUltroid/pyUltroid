75007521e0e2e546aaf9db962000417e988105f6
Danish <danish@ultroid.tech>
2021-11-22 05:48:38 +0530

    🥱

--- pyUltroid/misc/_supporter.py
+++ pyUltroid/misc/_supporter.py
@@ -36,41 +36,18 @@ black_list_chats = udB.get_key("BLACKLIST_CHATS")
 
 
 def admin_cmd(pattern=None, command=None, **args):
-    args["func"] = lambda e: not e.via_bot_id and not e.fwd_from
+    args["func"] = lambda e: not e.via_bot_id
     args["chats"] = black_list_chats
     args["blacklist_chats"] = True
-    previous_stack_frame = inspect.stack()[1]
-    file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
-    if pattern is not None:
-        args["pattern"] = re.compile(hndlr + pattern)
-        reg = re.compile("(.*)")
-        try:
-            cmd = re.search(reg, pattern)
-            try:
-                cmd = (
-                    cmd.group(1)
-                    .replace("$", "")
-                    .replace("?(.*)", "")
-                    .replace("(.*)", "")
-                    .replace("(?: |)", "")
-                    .replace("| ", "")
-                    .replace("( |)", "")
-                    .replace("?((.|//)*)", "")
-                    .replace("?P<shortname>\\w+", "")
-                )
-            except BaseException:
-                pass
-            try:
-                LIST[file_test].append(cmd)
-            except BaseException:
-                LIST.update({file_test: [cmd]})
-        except BaseException:
-            pass
     args["outgoing"] = True
-    args["incoming"] = False
-    if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        del args["allow_edited_updates"]
+    args["forwards"] = False
+    if pattern:
+        args["pattern"] = re.compile(hndlr + pattern)
+        file = Path(inspect.stack()[1].filename)
+        if LIST.get(file.stem):
+            LIST[file.stem].append(pattern)
+        else:
+            LIST.update({file.stem: [pattern]})
     return events.NewMessage(**args)
 
 
@@ -80,19 +57,15 @@ register = ultroid_cmd
 
 
 def sudo_cmd(allow_sudo=True, pattern=None, command=None, **args):
-    args["func"] = lambda e: not e.via_bot_id and not e.fwd_from
+    args["func"] = lambda e: not e.via_bot_id
     args["chats"] = black_list_chats
     args["blacklist_chats"] = True
-    previous_stack_frame = inspect.stack()[1]
-    file_test = Path(previous_stack_frame.filename)
-    file_test = file_test.stem.replace(".py", "")
+    args["forwards"] = False
     if pattern:
         args["pattern"] = re.compile("\\" + SUDO_HNDLR + pattern)
     if allow_sudo:
         args["from_users"] = SUDO_M.get_sudos
         args["incoming"] = True
-    if "allow_edited_updates" in args and args["allow_edited_updates"]:
-        del args["allow_edited_updates"]
     return events.NewMessage(**args)
 
 
@@ -110,23 +83,16 @@ class Config((object)):
         LOGGER = True
         LOCATION = os.environ.get("LOCATION", None)
         OPEN_WEATHER_MAP_APPID = os.environ.get("OPEN_WEATHER_MAP_APPID", None)
-        SCREEN_SHOT_LAYER_ACCESS_KEY = os.environ.get(
-            "SCREEN_SHOT_LAYER_ACCESS_KEY", None
-        )
         SUDO_COMMAND_HAND_LER = "\\" + SUDO_HNDLR
         TMP_DOWNLOAD_DIRECTORY = os.environ.get(
             "TMP_DOWNLOAD_DIRECTORY", "resources/downloads/"
         )
         TEMP_DOWNLOAD_DIRECTORY = TMP_DOWNLOAD_DIRECTORY
+        TEMP_DIR = TMP_DOWNLOAD_DIRECTORY
         TELEGRAPH_SHORT_NAME = os.environ.get("TELEGRAPH_SHORT_NAME", "Ultroid")
         OCR_SPACE_API_KEY = os.environ.get("OCR_SPACE_API_KEY", None)
         G_BAN_LOGGER_GROUP = BOT_LOG
-        GOOGLE_SEARCH_COUNT_LIMIT = int(os.environ.get("GOOGLE_SEARCH_COUNT_LIMIT", 9))
-        TG_GLOBAL_ALBUM_LIMIT = int(os.environ.get("TG_GLOBAL_ALBUM_LIMIT", 9))
-        TG_BOT_TOKEN_BF_HER = Var.BOT_TOKEN
         TG_BOT_USER_NAME_BF_HER = asst.me.username
-        DUAL_LOG = os.environ.get("DUAL_LOG", None)
-        MAX_MESSAGE_SIZE_LIMIT = 4095
         UB_BLACK_LIST_CHAT = [
             int(blacklist) for blacklist in udB.get_key("BLACKLIST_CHATS")
         ]
@@ -134,40 +100,15 @@ class Config((object)):
         ANTI_FLOOD_WARN_MODE = types.ChatBannedRights(
             until_date=None, view_messages=None, send_messages=True
         )
-        CHATS_TO_MONITOR_FOR_ANTI_FLOOD = []
         REM_BG_API_KEY = os.environ.get("REM_BG_API_KEY", None)
-        SLAP_USERNAME = os.environ.get("SLAP_USERNAME", None)
         GITHUB_ACCESS_TOKEN = os.environ.get("GITHUB_ACCESS_TOKEN", None)
         GIT_REPO_NAME = os.environ.get("GIT_REPO_NAME", None)
-        NO_P_M_SPAM = bool(os.environ.get("NO_P_M_SPAM", True))
-        MAX_FLOOD_IN_P_M_s = int(os.environ.get("MAX_FLOOD_IN_P_M_s", 3))
-        PM_LOG_GRP_ID = os.environ.get("PM_LOG_GRP_ID", None)
-        NC_LOG_P_M_S = bool(os.environ.get("NC_LOG_P_M_S", True))
-        HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-        HEROKU_API_KEY = Var.HEROKU_API
         PRIVATE_GROUP_BOT_API_ID = BOTLOG
         PM_LOGGR_BOT_API_ID = BOTLOG
         DB_URI = os.environ.get("DATABASE_URL", None)
-        NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD = int(
-            os.environ.get("NO_OF_BUTTONS_DISPLAYED_IN_H_ME_CMD", 7)
-        )
-        NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD = int(
-            os.environ.get("NO_OF_COLOUMS_DISPLAYED_IN_H_ME_CMD", 3)
-        )
-        EMOJI_TO_DISPLAY_IN_HELP = os.environ.get("EMOJI_TO_DISPLAY_IN_HELP", "🔰")
         HANDLR = hndlr
         SUDO_USERS = SUDO_M.get_sudos()
-        GROUP_REG_SED_EX_BOT_S = os.environ.get(
-            "GROUP_REG_SED_EX_BOT_S", r"(regex|moku|BananaButler_|rgx|l4mR)bot"
-        )
-        TEMP_DIR = os.environ.get("TEMP_DIR", None)
         CHANNEL_ID = int(os.environ.get("CHANNEL_ID", -100))
-        CHROME_DRIVER = os.environ.get(
-            "CHROME_DRIVER", "/app/.chromedriver/bin/chromedriver"
-        )
-        GOOGLE_CHROME_BIN = os.environ.get(
-            "GOOGLE_CHROME_BIN", "/app/.apt/usr/bin/google-chrome"
-        )
         BLACKLIST_CHAT = UB_BLACK_LIST_CHAT
         MONGO_URI = os.environ.get("MONGO_URI", None)
         ALIVE_PHOTTO = os.environ.get("ALIVE_PHOTTO", None)
