83170392a4470f92e353c09ab46e73d70a1631ce
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-23 11:26:23 +0530

    Update gban_mute_db.py

--- pyUltroid/dB/gban_mute_db.py
+++ pyUltroid/dB/gban_mute_db.py
@@ -8,69 +8,49 @@
 from .. import udB
 
 
-def get_gban_stuff():
-    a = udB.get_key("GBAN")
-    if not a:
-        return {}
-    try:
-        return dict(eval(a))
-    except BaseException:
-        udB.del_key("GBAN")
+def list_gbanned():
+    if x := udB.get_key("GBAN"):
+        return x
     return {}
-
-
-ge = udB.get_key("GMUTE")
-if ge:
-    try:
-        if "list" not in str(type(eval(ge))):
-            udB.set_key("GMUTE", "[]")
-    except BaseException:
-        udB.set_key("GMUTE", "[]")
 
 
 def gban(user, reason):
     ok = list_gbanned()
     ok.update({user: reason or "No Reason. "})
-    udB.set_key("GBAN", str(ok))
+    return udB.set_key("GBAN", ok)
 
 
 def ungban(user):
     ok = list_gbanned()
     if ok.get(user):
         del ok[user]
-        udB.set_key("GBAN", str(ok))
+        return udB.set_key("GBAN", str(ok))
 
 
 def is_gbanned(user):
     ok = list_gbanned()
     if ok.get(user):
         return ok[user]
-    return False
-
-
-def list_gbanned():
-    return get_gban_stuff()
 
 
 def gmute(user):
     ok = list_gmuted()
     ok.append(user)
-    udB.set_key("GMUTE", str(ok))
+    return udB.set_key("GMUTE", ok)
 
 
 def ungmute(user):
     ok = list_gmuted()
     if user in ok:
         ok.remove(user)
-        udB.set_key("GMUTE", str(ok))
+        return udB.set_key("GMUTE", ok)
 
 
 def is_gmuted(user):
-    ok = list_gmuted()
-    return user in ok
+    return user in list_gmuted()
 
 
 def list_gmuted():
-    if udB.get_key("GMUTE"):
-        return eval(udB.get_key("GMUTE"))
+    if x := udB.get_key("GMUTE"):
+        return x
     return []
