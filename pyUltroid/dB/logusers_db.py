4fae78ecc566271daf08e2d7fd0f392c0b49a7ce
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-25 19:59:00 +0530

    Update logusers_db.py

--- pyUltroid/dB/logusers_db.py
+++ pyUltroid/dB/logusers_db.py
@@ -8,52 +8,21 @@
 from .. import udB
 
 
-def str_to_list(text):  # Returns List
-    return text.split(" ")
+def get_logger():
+    return udB.get_key("LOGUSERS") or []
 
 
-def list_to_str(list_):  # Returns String
-    str_ = "".join(f"{x} " for x in list_)
-    return str_.strip()
+def is_logger(id_):
+    return id_ in get_logger()
 
 
-def get_logger():  # Returns List
-    pmperm = udB.get_key("LOGUSERS")
-    if not pmperm:
-        return [""]
-    return str_to_list(pmperm)
-
-
-def is_logger(id_):  # Take int or str with numbers only , Returns Boolean
-    if not str(id_).isdigit():
-        return False
-    pmperm = get_logger()
-    return str(id_) in pmperm
-
-
-def log_user(id_):  # Take int or str with numbers only , Returns Boolean
-    id_ = str(id_)
-    if not id_.isdigit():
-        return False
-    try:
+def log_user(id_):
         pmperm = get_logger()
         pmperm.append(id_)
-        udB.set_key("LOGUSERS", list_to_str(pmperm))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/logusers_db/log_user : {e}")
-        return False
+        return udB.set_key("LOGUSERS", pmperm)
 
 
-def nolog_user(id_):  # Take int or str with numbers only , Returns Boolean
-    id_ = str(id_)
-    if not id_.isdigit():
-        return False
-    try:
+def nolog_user(id_):
         pmperm = get_logger()
         pmperm.remove(id_)
-        udB.set_key("LOGUSERS", list_to_str(pmperm))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/loguser_db/nolog_user : {e}")
-        return False
+        return udB.set_key("LOGUSERS", pmperm)
