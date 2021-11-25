a15eaf778de942d55436755e6551051b74f07bc3
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-25 19:38:26 +0530

    Skem

--- pyUltroid/dB/vc_sudos.py
+++ pyUltroid/dB/vc_sudos.py
@@ -8,57 +8,23 @@
 from .. import udB
 
 
-def str_to_list(text):  # Returns List
-    return text.split(" ")
+def get_vcsudos():
+    return udB.get_key("VC_SUDOS") or []
 
 
-def list_to_str(list):  # Returns String
-    str = "".join(f"{x} " for x in list)
-    return str.strip()
+def is_vcsudo(id):
+    return id in get_sudos()
 
 
-def are_all_nums(list):  # Takes List , Returns Boolean
-    return all(item.isdigit() for item in list)
-
-
-def get_vcsudos():  # Returns List
-    sudos = udB.get_key("VC_SUDOS")
-    if sudos is None or sudos == "":
-        return [""]
-    else:
-        return str_to_list(sudos)
-
-
-def is_vcsudo(id):  # Take int or str with numbers only , Returns Boolean
-    if not str(id).isdigit():
-        return False
-    sudos = get_vcsudos()
-    return str(id) in sudos
-
-
-def add_vcsudo(id):  # Take int or str with numbers only , Returns Boolean
-    id = str(id)
-    if not id.isdigit():
-        return False
-    try:
+def add_vcsudo(id):
         sudos = get_vcsudos()
         sudos.append(id)
-        udB.set_key("VC_SUDOS", list_to_str(sudos))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/sudos/add_sudo : {e}")
-        return False
+        return udB.set_key("VC_SUDOS", sudos)
 
 
-def del_vcsudo(id):  # Take int or str with numbers only , Returns Boolean
-    id = str(id)
-    if not id.isdigit():
-        return False
-    try:
+def del_vcsudo(id):
+    if is_vcsudo(id):
         sudos = get_vcsudos()
         sudos.remove(id)
-        udB.set_key("VC_SUDOS", list_to_str(sudos))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/sudos/del_sudo : {e}")
-        return False
+        return udB.set_key("VC_SUDOS", sudos)
+    return True
