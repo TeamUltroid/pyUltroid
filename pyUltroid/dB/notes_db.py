b78c874a844a5d631337f49047c03c79f02cadd8
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-25 19:50:18 +0530

    Update notes_db.py

--- pyUltroid/dB/notes_db.py
+++ pyUltroid/dB/notes_db.py
@@ -9,14 +9,7 @@ from .. import udB
 
 
 def get_stuff():
-    a = udB.get_key("NOTE")
-    if not a:
-        return {}
-    try:
-        return eval(a)
-    except BaseException:
-        udB.del_key("NOTE")
-    return {}
+    return udB.get_key("NOTE") or {}
 
 
 def add_note(chat, word, msg, media, button):
@@ -25,32 +18,30 @@ def add_note(chat, word, msg, media, button):
         ok[chat].update({word: {"msg": msg, "media": media, "button": button}})
     else:
         ok.update({chat: {word: {"msg": msg, "media": media, "button": button}}})
-    udB.set_key("NOTE", str(ok))
+    udB.set_key("NOTE", ok)
 
 
 def rem_note(chat, word):
     ok = get_stuff()
     if ok.get(chat) and ok[chat].get(word):
         ok[chat].pop(word)
-        udB.set_key("NOTE", str(ok))
+        return udB.set_key("NOTE", ok)
 
 
 def rem_all_note(chat):
     ok = get_stuff()
     if ok.get(chat):
         ok.pop(chat)
-        udB.set_key("NOTE", str(ok))
+        return udB.set_key("NOTE", ok)
 
 
 def get_notes(chat, word):
     ok = get_stuff()
     if ok.get(chat) and ok[chat].get(word):
         return ok[chat][word]
-    return False
 
 
 def list_note(chat):
     ok = get_stuff()
     if ok.get(chat):
         return "".join(f"👉 #{z}\n" for z in ok[chat])
-    return False
