0fb66db99cb5bcc00da99fe0a7f6e25415dea87e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-25 20:35:41 +0530

    Update asstcmd_db.py

--- pyUltroid/dB/asstcmd_db.py
+++ pyUltroid/dB/asstcmd_db.py
@@ -10,28 +10,20 @@ from .. import udB
 
 
 def get_stuff():
-    a = udB.get_key("ASST_CMDS")
-    if not a:
-        return {}
-    try:
-        return eval(a)
-    except BaseException:
-        udB.del_key("ASST_CMDS")
-    return {}
+    return udB.get_key("ASST_CMDS") or {}
 
 
 def add_cmd(cmd, msg, media, button):
     ok = get_stuff()
     ok.update({cmd: {"msg": msg, "media": media, "button": button}})
-    return udB.set_key("ASST_CMDS", str(ok))
+    return udB.set_key("ASST_CMDS", ok)
 
 
 def rem_cmd(cmd):
     ok = get_stuff()
     if ok.get(cmd):
         ok.pop(cmd)
-        return udB.set_key("ASST_CMDS", str(ok))
-    return
+        return udB.set_key("ASST_CMDS", ok)
 
 
 def cmd_reply(cmd):
@@ -44,6 +36,4 @@ def cmd_reply(cmd):
 
 def list_cmds():
     ok = get_stuff()
-    if ok.keys():
-        return ok.keys()
-    return []
+    return ok.keys()
