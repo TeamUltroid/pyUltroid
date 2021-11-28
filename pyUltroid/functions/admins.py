30be76fa9b5009c074350b109fd539f59d938311
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-11-28 06:53:05 +0530

    Update admins.py

--- pyUltroid/functions/admins.py
+++ pyUltroid/functions/admins.py
@@ -6,7 +6,7 @@
 # <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
 
 import time
-
+import uuid
 from telethon.errors.rpcerrorlist import UserNotParticipantError
 from telethon.tl import functions, types
 
@@ -39,14 +39,36 @@ async def ban_time(event, time_str):
 
 # ------------------Admin Check--------------- #
 
+async def _callback_check(event):
+    id_ = uuid.uuid1().split("-")[0]
+    time = time.time()
+    msg = await event.reply("Click Below Button within 2 minutes to prove self as Admin!", buttons=Button.inline("Click Me", f"cc_{id_}"))
+    if not _ult_cache.get("admin_callback"):
+        _ult_cache.update({"admin_callback":{id_:None}})
+    else:
+        _ult_cache["admin_callback"].update({id_:None})
+    while not _ult_cache["admin_callback"].get(id_):
+        await asyncio.sleep(1)
+    if not _ult_cache.get("admin_callback", {}).get(id_):
+        await msg.delete()
+        return
+    key = _ult_cache.get("admin_callback", {}).get(id_)
+    del _ult_cache["admin_callback"][id_]
+    return key
 
-async def admin_check(event):
+async def admin_check(event, require=None):
+    if event.sender_id in SUDO_M.owner_and_sudos():
+        return True
+    callback = None
+
     # for Anonymous Admin Support
     if (
         isinstance(event.sender, (types.Chat, types.Channel))
         and event.sender_id == event.chat_id
     ):
-        return True
+        if not require:
+            return True
+        callback = True
     if isinstance(event.sender, types.Channel):
         if _ult_cache.get("LINKED_CHATS") and _ult_cache["LINKED_CHATS"].get(
             event.chat_id
@@ -67,11 +89,16 @@ async def admin_check(event):
                 )
         if _ignore and event.sender.id == _ignore:
             return False
-        return True
-    if event.sender_id in SUDO_M.owner_and_sudos():
-        return True
+        callback = True
+    if callback:
+        get_ = await _callback_check(event)
+        if not get_:
+            return
+        user, perms = get_
+    else:
+        user = event.sender_id
     try:
-        perms = await event.client.get_permissions(event.chat_id, event.sender_id)
+        perms = await event.client.get_permissions(event.chat_id, user)
     except UserNotParticipantError:
         return False
     return isinstance(
@@ -89,7 +116,7 @@ def lock_unlock(query, lock=True):
      Is there any better way to do this?
     """
     rights = types.ChatBannedRights(None)
-    _do = not lock
+    _do = lock
     if query == "msgs":
         for i in ["send_messages", "invite_users", "pin_messages" "change_info"]:
             setattr(rights, i, _do)
