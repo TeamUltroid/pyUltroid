fc4439b9db960c32046f74e4fc5154419a99441d
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-01 09:53:49 +0530

    Update blacklist_chat_db.py

--- pyUltroid/dB/blacklist_chat_db.py
+++ pyUltroid/dB/blacklist_chat_db.py
@@ -2,14 +2,14 @@ from .. import udB
 
 
 def add_black_chat(chat_id):
-    chat = eval(udB.get_key("BLACKLIST_CHATS"))
+    chat = udB.get_key("BLACKLIST_CHATS")
     if chat_id not in chat:
         chat.append(chat_id)
-        udB.set_key("BLACKLIST_CHATS", str(chat))
+        return udB.set_key("BLACKLIST_CHATS", chat)
 
 
 def rem_black_chat(chat_id):
-    chat = eval(udB.get_key("BLACKLIST_CHATS"))
+    chat = udB.get_key("BLACKLIST_CHATS")
     if chat_id in chat:
         chat.remove(chat_id)
-        udB.set_key("BLACKLIST_CHATS", str(chat))
+        return udB.set_key("BLACKLIST_CHATS", chat)
