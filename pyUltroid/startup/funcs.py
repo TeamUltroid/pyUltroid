1a36bbcf6f95aef6f320cd8722c4fe4c48c9d309
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-10 19:48:30 +0530

    Create funcs.py

--- pyUltroid/startup/funcs.py
+++ pyUltroid/startup/funcs.py
@@ -345,23 +345,28 @@ async def plug(plugin_channels):
             async for x in ultroid_bot.iter_messages(
                 chat, search=".py", filter=InputMessagesFilterDocument, wait_time=10
             ):
-                plugin = x.file.name.replace("_", " ")
-                if not os.path.exists("addons"):
+                plugin = x.file.name
+                if plugin in os.listdir("addons"):
+                    try:
+                        load_addons(plugin.replace(".py", ""))
+                    except Exception as e:
+                        LOGS.info(f"Ultroid - PLUGIN_CHANNEL - ERROR - {plugin}")
+                        LOGS.exception(e)
+                        os.remove("addons/"+plugin)
+                else:
                     await asyncio.sleep(0.6)
-                    plugin = await x.download_media(f"addons/{plugin}")
-                try:
-                    load_addons(plugin.replace(".py", ""))
-                except Exception as e:
-                    LOGS.info(f"Ultroid - PLUGIN_CHANNEL - ERROR - {plugin}")
-                    LOGS.exception(e)
-                    os.remove(plugin)
-        except Exception as er:
-            LOGS.exception(er)
+                    file = await ultroid_bot.download_media(x.media, "./addons/")
+                    try:
+                        load_addons(plugin.replace(".py", ""))
+                    except Exception as e:
+                        LOGS.info(f"Ultroid - PLUGIN_CHANNEL - ERROR - {plugin}")
+                        LOGS.exception(e)
+                        os.remove(file)
+        except Exception as e:
+            LOGS.exception(e)
 
 
 # some stuffs
-
-
 async def ready():
     from .. import asst, udB, ultroid_bot
 
