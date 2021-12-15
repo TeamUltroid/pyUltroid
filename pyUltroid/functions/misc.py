b748777ab33fa5f9640c3cdf7edbc4148d53dde1
TeamUltroid <teamultroid@ultroid.tech>
2021-12-15 17:59:39 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/misc.py
+++ pyUltroid/functions/misc.py
@@ -501,8 +501,9 @@ def split_list(List, index):
 
 # https://stackoverflow.com/questions/9041681/opencv-python-rotate-image-by-x-degrees-around-specific-point
 
+
 def rotate_image(image, angle):
-  image_center = tuple(np.array(image.shape[1::-1]) / 2)
-  rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
-  result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
-  return result
+    image_center = tuple(np.array(image.shape[1::-1]) / 2)
+    rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
+    result = cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
+    return result
