554fc3899b7a9ebd63127d1686dc3f05b06e731a
TeamUltroid <teamultroid@ultroid.tech>
2021-12-20 16:47:20 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/misc.py
+++ pyUltroid/functions/misc.py
@@ -515,4 +515,3 @@ def rotate_image(image, angle):
     image_center = tuple(np.array(image.shape[1::-1]) / 2)
     rot_mat = cv2.getRotationMatrix2D(image_center, angle, 1.0)
     return cv2.warpAffine(image, rot_mat, image.shape[1::-1], flags=cv2.INTER_LINEAR)
- 
