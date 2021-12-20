360884aba852ba085d461f70974278ee26fee6ae
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-20 20:32:36 +0530

    After update?

---
+++ pyUltroid/functions/executor.py
@@ -0,0 +1,11 @@
+# Some low(sarcastic) level codes
+
+"""
+class Terminal:
+    def __init__(self):
+        self._cache = {}
+
+class Evaluator:
+    def __init__(self):
+        self._cache = {}
+"""
