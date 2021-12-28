52189a97897baf33f20dcb54cdd4c5f77f05ce60
Devesh Pal <newdev0@outlook.com>
2021-12-28 09:07:51 +0530

    Create _extra.py

---
+++ pyUltroid/startup/_extra.py
@@ -0,0 +1,19 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+# https://bugs.python.org/issue26789
+# 'open' not defined has been fixed in Python3.10
+# for other older versions, something need to be done.
+
+def _fix_logging(handler):
+    handler._builtin_open = open
+
+    def _new_open(something):
+        open_func = something._builtin_open
+        return open_func(self.baseFilename, self.mode)
+
+    setattr(handler, "_open", _new_open)
