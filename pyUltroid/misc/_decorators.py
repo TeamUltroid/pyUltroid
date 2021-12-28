a595a6db82151d47fd36c2a144031a5445d303a4
Devesh Pal <newdev0@outlook.com>
2021-12-28 19:10:23 +0530

    Let it live only for owner
    All doesn't live in log channel

--- pyUltroid/misc/_decorators.py
+++ pyUltroid/misc/_decorators.py
@@ -190,11 +190,12 @@ def ultroid_cmd(pattern=None, manager=False, **kwargs):
                         udB.get_key("LOG_CHANNEL"),
                         ftext,
                     )
-                await ult.eor(
-                    f"<b><a href={error_log.message_link}>[An error occurred]</a></b>",
-                    link_preview=False,
-                    parse_mode="html",
-                )
+                if ult.out:
+                    await ult.edit(
+                        f"<b><a href={error_log.message_link}>[An error occurred]</a></b>",
+                        link_preview=False,
+                        parse_mode="html",
+                    )
 
         cmd = None
         blacklist_chats = False
