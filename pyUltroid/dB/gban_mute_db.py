fcd2dda190addd11c32357eb8a3e15046650e95b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-28 15:39:57 +0530

    Update gban_mute_db.py

--- pyUltroid/dB/gban_mute_db.py
+++ pyUltroid/dB/gban_mute_db.py
@@ -14,38 +14,38 @@ def list_gbanned():
 
 def gban(user, reason):
     ok = list_gbanned()
-    ok.update({user: reason or "No Reason. "})
+    ok.update({int(user): reason or "No Reason. "})
     return udB.set_key("GBAN", ok)
 
 
 def ungban(user):
     ok = list_gbanned()
-    if ok.get(user):
-        del ok[user]
+    if ok.get(int(user)):
+        del ok[int(user)]
         return udB.set_key("GBAN", str(ok))
 
 
 def is_gbanned(user):
     ok = list_gbanned()
-    if ok.get(user):
-        return ok[user]
+    if ok.get(int(user)):
+        return ok[int(user)]
 
 
 def gmute(user):
     ok = list_gmuted()
-    ok.append(user)
+    ok.append(int(user))
     return udB.set_key("GMUTE", ok)
 
 
 def ungmute(user):
     ok = list_gmuted()
     if user in ok:
-        ok.remove(user)
+        ok.remove(int(user))
         return udB.set_key("GMUTE", ok)
 
 
 def is_gmuted(user):
-    return user in list_gmuted()
+    return int(user) in list_gmuted()
 
 
 def list_gmuted():
