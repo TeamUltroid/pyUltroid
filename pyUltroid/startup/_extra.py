4a634a5bb8e92d3ec83647c45f5706beda3828a0
Devesh Pal <newdev0@outlook.com>
2021-12-28 09:24:22 +0530

    Update _extra.py

--- pyUltroid/startup/_extra.py
+++ pyUltroid/startup/_extra.py
@@ -14,7 +14,7 @@ def _fix_logging(handler):
     handler._builtin_open = open
 
     def _new_open(self):
-        open_func = something._builtin_open
+        open_func = self._builtin_open
         return open_func(self.baseFilename, self.mode)
 
     setattr(handler, "_open", _new_open)
