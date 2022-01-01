e8db3efbf3c5491a7092c8c6b666237609153c9e
Devesh Pal <newdev0@outlook.com>
2022-01-01 15:06:27 +0530

    Update ch_db.py

--- pyUltroid/dB/ch_db.py
+++ pyUltroid/dB/ch_db.py
@@ -7,101 +7,62 @@
 
 from .. import udB
 
-
-def str_to_list(text):  # Returns List
-    return text.split(" ")
-
-
-def list_to_str(list_):  # Returns String
-    str_ = "".join(f"{x} " for x in list_)
-    return str_.strip()
-
-
-def are_all_num(list_):  # Takes List , Returns Boolean
-    return all(item.isdigit() for item in list_)
-
-
 def get_source_channels():  # Returns List
-    channels = udB.get_key("CH_SOURCE")
-    if not channels:
-        return [""]
-    return str_to_list(channels)
+    return udB.get_key("CH_SOURCE") or []
 
 
 def get_no_source_channels():  # Returns List
-    channels = udB.get_key("CH_SOURCE")
-    if not channels:
-        return 0
-    return len(channels.split(" "))
+    channels = udB.get_key("CH_SOURCE") or []
+    return len(channels)
 
 
 def is_source_channel_added(id_):
     channels = get_source_channels()
-    return str(id_) in channels
+    return id_ in channels
 
 
 def add_source_channel(id_):  # Take int or str with numbers only , Returns Boolean
-    id_ = str(id_)
-    try:
-        channels = get_source_channels()
+    channels = get_source_channels()
+    if id_ not in channels:
         channels.append(id_)
-        udB.set_key("CH_SOURCE", list_to_str(channels))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/broadcast_db/add_channel : {e}")
-        return False
-
+        udB.set_key("CH_SOURCE", channels)
+    return True
 
 def rem_source_channel(id_):
-    try:
-        channels = get_source_channels()
-        channels.remove(str(id_))
+    channels = get_source_channels()
+    if id_ in channels:
+        channels.remove(id_)
         udB.set_key("CH_SOURCE", list_to_str(channels))
-        return True
-    except Exception:
-        return False
-
+    return True
 
 #########################
 
 
 def get_destinations():  # Returns List
-    channels = udB.get_key("CH_DESTINATION")
-    if not channels:
-        return [""]
-    return str_to_list(channels)
+    return udB.get_key("CH_DESTINATION") or []
 
 
 def get_no_destinations():  # Returns List
-    channels = udB.get_key("CH_DESTINATION")
-    if not channels:
-        return 0
-    return len(channels.split(" "))
+    channels = udB.get_key("CH_DESTINATION") or []
+    return len(channels)
 
 
 def is_destination_added(id_):
     channels = get_destinations()
-    return str(id_) in channels
+    return id_ in channels
 
 
 def add_destination(id_):  # Take int or str with numbers only , Returns Boolean
-    id_ = str(id_)
-    try:
-        channels = get_destinations()
+    channels = get_destinations()
+    if id_ not in channels:
         channels.append(id_)
         udB.set_key("CH_DESTINATION", list_to_str(channels))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/broadcast_db/add_channel : {e}")
-        return False
+    return True
 
 
 def rem_destination(id_):
-    try:
-        channels = get_destinations()
-        channels.remove(str(id_))
+    channels = get_destinations()
+    if id_ in channels:
+        channels.remove(id_)
         udB.set_key("CH_DESTINATION", list_to_str(channels))
-        return True
-    except Exception as e:
-        print(f"Ultroid LOG : // functions/broadcast_db/rem_channel : {e}")
-        return False
+    return True
