89d8de92c4e6e0ea60d5f2b87b11486b9a4eb102
Devesh Pal <newdev0@outlook.com>
2022-01-02 14:39:24 +0530

    Update loader.py

--- pyUltroid/startup/loader.py
+++ pyUltroid/startup/loader.py
@@ -25,6 +25,8 @@ class Loader:
 
     def load(self, log=True, func=import_module, cmd_help=HELP, include=[], exclude=[]):
         if include:
+            if log:
+                self._logger.info("Including:{}".format("".join(f"• {name}" for name in include)))
             files = glob.glob(f"{self.path}/_*.py")
             for file in include:
                 path = f"{self.path}/{file}.py"
