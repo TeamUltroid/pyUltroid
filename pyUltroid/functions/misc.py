507c5975f74a464cf820e84e89c0e9483e2ce2ab
TeamUltroid <teamultroid@ultroid.tech>
2022-01-18 04:58:56 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/misc.py
+++ pyUltroid/functions/misc.py
@@ -478,6 +478,7 @@ async def create_quotly(
     if not isinstance(event, list):
         event = [event]
     from .. import udB
+
     url = "https://bot.lyo.su/quote/generate" if udB.get_key("OQAPI") else url
     bg = bg or "#1b1429"
     content = {
