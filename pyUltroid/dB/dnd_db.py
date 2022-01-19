ed8f2cff994d294b59cdba07440b385d71670049
TeamUltroid <teamultroid@ultroid.tech>
2022-01-19 17:31:16 +0000

    pyLint: auto-fixes

--- pyUltroid/dB/dnd_db.py
+++ pyUltroid/dB/dnd_db.py
@@ -12,15 +12,18 @@ from .. import udB
 def get_dnd_chats():
     return udB.get_key("DND_CHATS") or []
 
+
 def add_dnd(chat_id):
     x = get_dnd_chats()
     x.append(int(chat_id))
     return udB.set_key("DND_CHATS", x)
 
+
 def del_dnd(chat_id):
     x = get_dnd_chats()
     x.remove(int(chat_id))
     return udB.set_key("DND_CHATS", x)
+
 
 def chat_in_dnd(chat_id):
     return int(chat_id) in get_dnd_chats()
