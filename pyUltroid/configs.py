0cfdcdf5494c51aaaa04a7deecaa2cfa8a61801d
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-02-06 13:39:29 +0530

    Update configs.py

--- pyUltroid/configs.py
+++ pyUltroid/configs.py
@@ -9,9 +9,9 @@ import sys
 
 from decouple import config
 
-# from dotenv import find_dotenv, load_dotenv
+from dotenv import find_dotenv, load_dotenv
 
-# load_dotenv(find_dotenv())
+load_dotenv(find_dotenv())
 
 
 class Var:
