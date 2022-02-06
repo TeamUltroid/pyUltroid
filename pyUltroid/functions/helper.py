15fbacacb990ab9c08477deffee799687a773b7c
TeamUltroid <teamultroid@ultroid.tech>
2022-02-06 08:12:16 +0000

    pyLint: auto-fixes

--- pyUltroid/functions/helper.py
+++ pyUltroid/functions/helper.py
@@ -495,7 +495,18 @@ async def restart(ult):
         if len(sys.argv) == 1:
             os.execl(sys.executable, sys.executable, "-m", "pyUltroid")
         else:
-            os.execl(sys.executable, sys.executable, "-m", "pyUltroid", sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
+            os.execl(
+                sys.executable,
+                sys.executable,
+                "-m",
+                "pyUltroid",
+                sys.argv[1],
+                sys.argv[2],
+                sys.argv[3],
+                sys.argv[4],
+                sys.argv[5],
+                sys.argv[6],
+            )
 
 
 async def shutdown(ult):
