66bf0f903e5dc14c37845a7e65bc49ec14044962
Devesh Pal <newdev0@outlook.com>
2022-02-08 14:37:38 +0530

    Update ytdl.py

--- pyUltroid/functions/ytdl.py
+++ pyUltroid/functions/ytdl.py
@@ -71,7 +71,7 @@ async def download_yt(event, link, ytd):
                 file, show_progress=True, event=event, to_delete=True
             )
             from_ = info["extractor"].split(":")[0]
-            caption = f"`[{num}/{total}]` `{title}`\n\n`From {from_}`"
+            caption = f"`[{num}/{total}]` `{title}`\n\n`from {from_}`"
             await event.client.send_file(
                 event.chat_id,
                 file=res,
