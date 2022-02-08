1153323a0c3fb408bc9f30300868361848a8e16c
Devesh Pal <newdev0@outlook.com>
2022-02-08 14:02:20 +0530

    Hey God of variable naming, please cooperate

--- pyUltroid/functions/ytdl.py
+++ pyUltroid/functions/ytdl.py
@@ -68,7 +68,7 @@ async def download_yt(event, link, ytd):
                 file, show_progress=True, event=event, to_delete=True
             )
             from_ = info["extractor"].split(":")[0]
-            caption = f"`[{num}/{total}]` `{title}`\n\n`From {_from}`"
+            caption = f"`[{num}/{total}]` `{title}`\n\n`From {from_}`"
             await event.client.send_file(
                 event.chat_id,
                 file=res,
