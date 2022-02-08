5f82db52cee31ae14ccb03eae57331b17bffb1a5
Devesh Pal <newdev0@outlook.com>
2022-02-08 22:21:48 +0530

    Update _extra.py

--- pyUltroid/startup/_extra.py
+++ pyUltroid/startup/_extra.py
@@ -18,3 +18,9 @@ def _fix_logging(handler):
         return open_func(self.baseFilename, self.mode)
 
     setattr(handler, "_open", _new_open)
+
+def _ask_input():
+    # Ask for Input even on Vps and other platforms.
+    def new_input(*args, **kwargs):
+        raise EOFError("args="str(args) + ", kwargs=" + str(kwargs))
+    __builtins__["input"] == new_input
