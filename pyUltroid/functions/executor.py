550f4233b95e155816dc9fa1ae833f031119882f
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-02-15 14:29:54 +0530

    Update executor.py

--- pyUltroid/functions/executor.py
+++ pyUltroid/functions/executor.py
@@ -1,5 +1,35 @@
+from psutil import Process
+from asyncio import create_subprocess_shell, subprocess
+
+
 class Terminal:
-    pass
+    """Hurrr"""
+    def __init__(self):
+        self._processes = {}
+
+    @property
+    def to_str(data):
+        return data.decode("utf-8")
+
+    async def run(self, cmd):
+        process = await create_subprocess_shell(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        pid = process.pid
+        self._process[pid] = process
+        return pid
+
+    async def terminate(self, pid):
+        try:
+            self._processes.pop(pid)
+            return await self._processes[pid].kill()
+        except KeyError:
+            return None
+
+    async def output(self, pid):
+        return self.to_str(await self._processes[pid].stdout.read())
+
+    async def error(self, pid):
+        return self.to_str(await self._processes[pid].stderr.read())
+
 
 class Executor:
     pass
