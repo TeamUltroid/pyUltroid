72e38bd332e50efb65094b929fd167873318ec9f
Devesh Pal <newdev0@outlook.com>
2022-02-15 14:22:59 +0530

    Update __main__.py

--- pyUltroid/__main__.py
+++ pyUltroid/__main__.py
@@ -51,7 +51,7 @@ addons = udB.get_key("ADDONS") or Var.ADDONS
 vcbot = udB.get_key("VCBOT") or Var.VCBOT
 
 if HOSTED_ON == "termux" and udB.get_key("EXCLUDE_OFFICIAL") == None:
-    _plugins = "gdrive pdftools writer"
+    _plugins = "autocorrect compressor gdrive instagram pdftools writer"
     udB.set_key("EXCLUDE_OFFICIAL", _plugins)
 
 load_other_plugins(addons=addons, pmbot=pmbot, manager=manager, vcbot=vcbot)
