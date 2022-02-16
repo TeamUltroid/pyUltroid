4e21dfe42d7d45ba9c3e5c5de604b0b5322b1b9a
Devesh Pal <newdev0@outlook.com>
2022-02-16 13:02:26 +0530

    Update tools.py

--- pyUltroid/functions/tools.py
+++ pyUltroid/functions/tools.py
@@ -113,17 +113,17 @@ def _unquote_text(text):
     return text.replace("'", "'").replace('"', '"')
 
 
-def json_parser(data, indent=None):
+def json_parser(data, indent=None, ascii=False):
     parsed = {}
     try:
         if isinstance(data, str):
             parsed = json.loads(str(data))
             if indent:
-                parsed = json.dumps(json.loads(str(data)), indent=indent)
+                parsed = json.dumps(json.loads(str(data)), indent=indent, ensure_ascii=ascii)
         elif isinstance(data, dict):
             parsed = data
             if indent:
-                parsed = json.dumps(data, indent=indent)
+                parsed = json.dumps(data, indent=indent, ensure_ascii=ascii)
     except JSONDecodeError:
         parsed = eval(data)
     return parsed
