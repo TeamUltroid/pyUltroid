144a16a556e1131849883293d66124cbe1b8cd74
Devesh Pal <newdev0@outlook.com>
2022-02-22 09:59:26 +0530

    Update __main__.py

--- pyUltroid/__main__.py
+++ pyUltroid/__main__.py
@@ -1,5 +1,13 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
+
 if __name__ == "__main__":
-    from .main import main
+    from .main import main, asst
 
     main()
     # start loop
