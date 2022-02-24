b3cf28ca3ca3132873f652c1a18a060601081842
Devesh Pal <newdev0@outlook.com>
2022-02-24 14:24:32 +0530

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,10 @@ author = "TeamUltroid"
 release = "0.1"
 version = "0.1.0"
 
-extensions = ["myst_parser"]
+extensions = ["myst_parser","sphinxext.opengraph"]
+
+ogp_site_url = "https://pyUltroid.readthedocs.io"
+
 # -- Options for HTML output
 
 html_theme = "sphinx_rtd_theme"
