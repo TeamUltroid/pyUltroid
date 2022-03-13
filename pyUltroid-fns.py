a812f9d0b956bdc44ce29a31b70105405e725bf4
Devesh Pal <newdev0@outlook.com>
2022-03-13 18:47:19 +0530

    Update pyUltroid-fns.py

--- pyUltroid-fns.py
+++ pyUltroid-fns.py
@@ -3,8 +3,8 @@ import shutil
 
 import setuptools
 
-# with open("README.md", "r", encoding="utf-8") as fh:
-#     long_description = fh.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 long_description = "# pyUltroid-fns"
 
