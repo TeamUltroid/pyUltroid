88ae3ec51e4e66151061ea07da20e1dd5cda9ecf
Devesh Pal <newdev0@outlook.com>
2022-03-24 11:01:20 +0530

    Update tools.py

--- pyUltroid/functions/tools.py
+++ pyUltroid/functions/tools.py
@@ -545,9 +545,14 @@ async def Carbon(
     rayso=False,
     **kwargs,
 ):
-    kwargs["code"] = code
     if rayso and not base_url:
-        base_url = "https://rayso.onrender.com/generate"
+        base_url = "https://raysoapi.herokuapp.com/generate"
+        kwargs["text"] = code
+        kwargs["theme"] = kwargs.get("theme", "meadow")
+        kwargs["darkMode"] = kwargs.get("darkMode", True)
+        kwargs["title"] = kwargs.get("title", "Ultroid")
+    else:
+        kwargs["code"] = code
     con = await async_searcher(base_url, post=True, json=kwargs, re_content=True)
     if not download:
         file = BytesIO(con)
