671983927c772996083d899b8c6fe093f5a6f2d0
Devesh Pal <newdev0@outlook.com>
2022-07-08 22:39:37 +0530

    Update connections.py

--- pyUltroid/startup/connections.py
+++ pyUltroid/startup/connections.py
@@ -45,12 +45,12 @@ def validate_session(session, logger):
     sys.exit()
 
 
-def vc_connection(udB, ultroid_bot, logs):
+def vc_connection(udB, ultroid_bot):
     VC_SESSION = Var.VC_SESSION or udB.get_key("VC_SESSION")
     if VC_SESSION and VC_SESSION != Var.SESSION:
         try:
             return UltroidClient(
-                validate_session(VC_SESSION, logs), log_attempt=False, handle_auth_error=False
+                validate_session(VC_SESSION, LOGS), log_attempt=False, handle_auth_error=False
             )
         except (AuthKeyDuplicatedError, EOFError):
             LOGS.info(
