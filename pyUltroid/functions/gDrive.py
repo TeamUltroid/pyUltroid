078b1942745cb793a2d04f59a2b2133bc67c8ed3
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-07-12 11:57:49 +0530

    Update gDrive.py

--- pyUltroid/functions/gDrive.py
+++ pyUltroid/functions/gDrive.py
@@ -20,7 +20,6 @@ from oauth2client.file import Storage
 from .. import udB
 from .helper import humanbytes, time_formatter
 
-_flow = {}
 
 for log in [LOGGER, logger, _logger]:
     log.setLevel(WARNING)
@@ -28,6 +27,7 @@ for log in [LOGGER, logger, _logger]:
 
 class GDriveManager:
     def __init__(self):
+        self._flow = {}
         self.gdrive_creds = {
             "oauth_scope": [
                 "https://www.googleapis.com/auth/drive",
@@ -50,8 +50,8 @@ class GDriveManager:
         return f"https://drive.google.com/folderview?id={folderId}"
 
     def _create_token_file(self, code: str = None):
-        if code and _flow:
-            _auth_flow = _flow["_"]
+        if code and self._flow:
+            _auth_flow = self._flow["_"]
             credentials = _auth_flow.step2_exchange(code)
             Storage(self.token_file).put(credentials)
             return udB.set_key("GDRIVE_AUTH_TOKEN", str(open(self.token_file).read()))
@@ -64,7 +64,7 @@ class GDriveManager:
                 self.gdrive_creds["oauth_scope"],
                 redirect_uri=self.gdrive_creds["redirect_uri"],
             )
-            _flow["_"] = _auth_flow
+            self._flow["_"] = _auth_flow
         except KeyError:
             return "Fill GDRIVE client credentials"
         return _auth_flow.step1_get_authorize_url()
